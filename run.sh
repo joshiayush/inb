4a0bb68cfab7e83d24a3e9437dd77d1e30a696d9
Ayush Joshi <ayush854032@gmail.com>
2021-02-02 11:44:12 +0530

    added shell script to execute linkedin automater program and also sets the requirements if not present

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # * The line at the head of the script tells the shell that it is a shell
-# * script and it contains commands in it and you need to execute it 
+# * script and it contains commands in it and you need to execute it
 # ? #!/bin/sh
 # * Executes the script using the Bourne shell or a compatible shell, with path /bin/sh
 # ? #!/bin/bash
@@ -31,11 +31,59 @@ function confirm() {
     echo $ch
 }
 
+# ! function that finds the system platform Information
+function getSystemInfo() {
+    case "$(/usr/bin/lsb_release -si)" in
+    Ubuntu) echo "Ubuntu" ;;
+    Mint) echo "Mint" ;;
+    arch) echo "Arch" ;;
+    *) echo "Can not get the System Information! Download Python manually" ;;
+    esac
+}
+
+# ! function that install python on Ubuntu platform
+function installOnUbuntu() {
+    echo "Installing Python on Ubuntu Linux"
+    sudo apt-get update
+    sudo apt-get install python3.8 python3-pip
+}
+
+# ! function that install python on Linux Mint platform
+function installOnMint() {
+    echo "Installing Python on Linux Mint"
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt-get update
+    sudo apt-get install python3.8 python3-pip
+}
+
+# ! function that install python on Arch Linux platform
+function installOnArch() {
+    echo "Installing Python on Arch Linux"
+    packman -S python
+}
+
+# ? function that does the actual installation of the python interpreter
+# ? based on the current running platform
+function install() {
+    platform=$(getSystemInfo)
+
+    case "$platform" in
+    Ubuntu) installOnUbuntu ;;
+    Mint) installOnMint ;;
+    Arch) installOnArch ;;
+    *) exit ;;
+    esac
+}
+
 # ? function that install python if it is not already installed
 # ? in the user system
 function installPython() {
-    if []; then
+    # ! get confirmation
+    ch=$(confirm)
+
+    if [ "$ch" = "y" ]; then
         echo "Installing python for $(uname -s)"
+        install
     else
         echo "Piece"
     fi
