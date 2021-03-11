ab484e3b2c7d041cccec81a2abe4bcc95ce69fb6
Ayush Joshi <ayush854032@gmail.com>
2021-03-11 16:49:33 +0530

    I was just trying the git commit editior :)

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,7 +1,8 @@
-# Package db is to manage all the Google Cloud Firebase authentication, initialization,
-# database manipulation for our linkedin bot we will be adding a lot of data in the firestore
-# to train our machine learning model (that I'll be adding soon) to recognize First Name and
-# Last Name, Female's and Male's name and the dataset for our chatbot.
+"""Package db is to manage all the Google Cloud Firebase authentication, initialization,
+database manipulation for our linkedin bot we will be adding a lot of data in the firestore
+to train our machine learning model (that I'll be adding soon) to recognize First Name and
+Last Name, Female's and Male's name and the dataset for our chatbot.
+"""
 
 __COLLECTION_CONFIGS__ = "configs"
 __CONFIGS_DOC_LINKEDIN__ = "F5pKooUFtV27sfDhCWJK"
