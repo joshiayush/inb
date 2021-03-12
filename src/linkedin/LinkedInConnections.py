af2263b7e98e2231a27c9aa3ea304d948e2d10e0
Ayush Joshi <ayush854032@gmail.com>
2021-03-12 11:18:19 +0530

    added send_input, search_for_result_entities, click_send_invitation_button functionality to class LinkedInConnectionsAutoSearch\nIssue: stale element reference when clicking the send now button after the send invitation button

--- src/linkedin/LinkedInConnections.py
+++ src/linkedin/LinkedInConnections.py
@@ -214,10 +214,17 @@ class LinkedInConnectionsAuto(LinkedIn):
         except NoSuchElementException:
             pass
 
+    def clear_overlapping_elements(self):
+        self.driver.execute_script((
+            """document.querySelector("div[class^='msg-overlay-list-bubble']").style = 'display: none';"""
+        ))
+
     def run(self):
         """Function run() is the main function from where the program
         starts doing its job.
         """
+        self.clear_overlapping_elements()
+
         self.get_my_network()
 
         self.prepare_page()
@@ -228,17 +235,66 @@ class LinkedInConnectionsAuto(LinkedIn):
 class LinkedInConnectionsAutoSearch(LinkedIn):
     def __init__(self, data):
         super(LinkedInConnectionsAutoSearch, self).__init__(data)
-        self.init_vars()
 
-    def init_vars(self):
-        self.connections_link = ""
+    def send_input_keywords(self):
+        self.driver.find_element_by_css_selector(
+            "input[aria-label='Search']").send_keys(self.data["search_keywords"], Keys.ENTER)
 
-    def quote_url(self, url, safe=f"~@#$&()*!+=:;,.?/\\"):
-        """URL-encodes a string (either str (i.e. ASCII) or unicode)
-        uses de-facto UTF-8 encoding to handle Unicode codepoints in
-        given string.
-        """
-        return urllib.parse.quote(url.encode('utf-8'), safe)
+    def select_category_people(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                expected_conditions.presence_of_element_located(
+                    (By.CSS_SELECTOR, "button[aria-label^='People']")
+                )
+            )
+            self.driver.find_element_by_css_selector("button[aria-label^='People']").click()
+        except TimeoutException:
+            pass
+
+    def apply_search_location(self):
+        pass
+
+    def get_entity_results(self):
+        try:
+            WebDriverWait(self.driver, 10).until(
+                expected_conditions.presence_of_element_located(
+                    (By.CSS_SELECTOR, "div[class^='entity-result__actions']")
+                )
+            )
+            return self.driver.find_elements_by_css_selector("div[class^='entity-result__actions']")
+        except TimeoutException:
+            pass
+
+    def start_connecting(self):
+        while_element_not_interactable = False
+
+        for result in self.get_entity_results():
+            try:
+                result.find_element_by_tag_name("button").click()
+                try:
+                    WebDriverWait(self.driver, 10).until(
+                        expected_conditions.presence_of_element_located(
+                            (By.CSS_SELECTOR, "button[aria-label^='Send now']")
+                        )
+                    )
+                    self.driver.find_element_by_css_selector("button[aria-label^='Send now']").click()
+                except ElementClickInterceptedException:
+                    pass
+            except ElementNotInteractableException:
+                while_element_not_interactable = True
+
+        if while_element_not_interactable == False:
+            self.driver.find_element_by_css_selector(
+                "button[aria-label^='Next']").click()
+
+            self.start_connecting()
 
     def run(self):
-        pass
+        self.send_input_keywords()
+
+        self.select_category_people()
+
+        if self.data["search_location"]:
+            pass
+
+        self.start_connecting()
