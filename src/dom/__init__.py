1da86efe4d6a6ef53118775cc59f1e7b3fb8a619
Ayush Joshi <ayush854032@gmail.com>
2021-04-30 18:13:42 +0530

    refactor (dom javascript): separate module for javascript execution
    Function get_page_y_offset() returns the window.pageYOffset
    of the webpage, we need that so we can keep on scrolling untill
    the page offset becomes constant. Declaration of this method is
    static because we want to use this function across multiple
    classes.
    
    Args:
        self: is not a object here but it is a parameter object
        that has a property 'driver' and we need that
    
    return:
        window.pageYOffset
    
    Function execute_javascript() scrolls the web page to the
    very bottom of it using the 'document.scrollingElement.scrollTop'
    property.
    
    Args:
        self: it is a parameter object that has a property 'driver'
        in it and we need that to access the webpage.

---
+++ src/dom/__init__.py
@@ -0,0 +1,4 @@
+"""Module dom contains all the neccessary functionalities require to deal with the dom elements."""
+
+__name__ = "DOM"
+__package__ = "DOM"
---
+++ src/dom/javascript.py
@@ -0,0 +1,40 @@
+def get_page_y_offset(self):
+    """Function get_page_y_offset() returns the window.pageYOffset
+    of the webpage, we need that so we can keep on scrolling untill
+    the page offset becomes constant. Declaration of this method is
+    static because we want to use this function across multiple
+    classes.
+
+    Args:
+        self: is not a object here but it is a parameter object
+        that has a property 'driver' and we need that
+
+    return:
+        window.pageYOffset
+    """
+    if not hasattr(self, "driver"):
+        return
+
+    return self.driver.execute_script((
+        "return (window.pageYOffset !== undefined)"
+        "       ? window.pageYOffset"
+        "       : (document.documentElement || document.body.parentNode || document.body);"
+    ))
+
+
+def execute_javascript(self):
+    """Function execute_javascript() scrolls the web page to the 
+    very bottom of it using the 'document.scrollingElement.scrollTop' 
+    property.
+
+    Args:
+        self: it is a parameter object that has a property 'driver' 
+        in it and we need that to access the webpage.
+    """
+    if not hasattr(self, "driver"):
+        return
+
+    self.driver.execute_script((
+        "var scrollingElement = (document.scrollingElement || document.body);"
+        "scrollingElement.scrollTop = scrollingElement.scrollHeight;"
+    ))
