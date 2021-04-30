6b111dcc7557d28e3e27edbf7a9b831bc125a217
Ayush Joshi <ayush854032@gmail.com>
2021-04-30 16:55:25 +0530

    feat (terminal): add printWhite() function for white text
    add printWhite() function for printing white text on the screen, as we use custom print
    function to print on the terminal window we also need to have a specific function that
    prints regular text on the screen otherwise we have to write extra code every time we
    want a regular text on the screen.

--- src/helpers/print.py
+++ src/helpers/print.py
@@ -1,8 +1,8 @@
 import colorama
 
 
-def _print(_str, **kwargs):
-    """Function _print() takes a string and displays that string on the terminal.
+def printk(_str, **kwargs):
+    """Function printk() takes a string and displays that string on the terminal.
     This function also adds left 'whitespace' padding to the given string if there is a
     'pad' property present in the 'kwargs' otherwise it simply displays that string.
 
@@ -23,6 +23,43 @@ def _print(_str, **kwargs):
     print(f"""{_str}""", **kwargs)
 
 
+def printWhite(_str, **kwargs):
+    """Function printWhite() takes a string and displays that string in WHITEcolor.
+
+    Args:
+        string: it is the string that needs to be printed on the terminal.
+        **kwargs: are the key-value pair that print method takes besides string.
+    """
+    if not kwargs:
+        printk(f"""{colorama.Fore.WHITE}{_str}{colorama.Fore.RESET}""")
+        return
+
+    if not "style" in kwargs:
+        printk(f"""{colorama.Fore.WHITE}{_str}{colorama.Fore.RESET}""", **kwargs)
+        return
+
+    if kwargs["style"].lower() == "b" or kwargs["style"].lower() == "bright":
+        del kwargs["style"]
+        print(f"""{colorama.Style.BRIGHT}""", end="")
+        printWhite(_str, **kwargs)
+        print(f"""{colorama.Style.RESET_ALL}""", end="")
+        return
+
+    if kwargs["style"].lower() == "n" or kwargs["style"].lower() == "normal":
+        del kwargs["style"]
+        print(f"""{colorama.Style.NORMAL}""", end="")
+        printWhite(_str, **kwargs)
+        print(f"""{colorama.Style.RESET_ALL}""", end="")
+        return
+
+    if kwargs["style"].lower() == "d" or kwargs["style"].lower() == "dim":
+        del kwargs["style"]
+        print(f"""{colorama.Style.DIM}""", end="")
+        printWhite(_str, **kwargs)
+        print(f"""{colorama.Style.RESET_ALL}""", end="")
+        return
+
+
 def printRed(_str, **kwargs):
     """Function printRed() takes a string and displays that string in red color.
 
@@ -31,11 +68,11 @@ def printRed(_str, **kwargs):
         **kwargs: are the key-value pair that print method takes besides string.
     """
     if not kwargs:
-        _print(f"""{colorama.Fore.RED}{_str}{colorama.Fore.RESET}""")
+        printk(f"""{colorama.Fore.RED}{_str}{colorama.Fore.RESET}""")
         return
 
     if not "style" in kwargs:
-        _print(f"""{colorama.Fore.RED}{_str}{colorama.Fore.RESET}""", **kwargs)
+        printk(f"""{colorama.Fore.RED}{_str}{colorama.Fore.RESET}""", **kwargs)
         return
 
     if kwargs["style"].lower() == "b" or kwargs["style"].lower() == "bright":
@@ -68,11 +105,11 @@ def printGreen(_str, **kwargs):
         **kwargs: are the key-value pair that print method takes besides string.
     """
     if not kwargs:
-        _print(f"""{colorama.Fore.GREEN}{_str}{colorama.Fore.RESET}""")
+        printk(f"""{colorama.Fore.GREEN}{_str}{colorama.Fore.RESET}""")
         return
 
     if not "style" in kwargs:
-        _print(f"""{colorama.Fore.GREEN}{_str}{colorama.Fore.RESET}""", **kwargs)
+        printk(f"""{colorama.Fore.GREEN}{_str}{colorama.Fore.RESET}""", **kwargs)
         return
 
     if kwargs["style"].lower() == "b" or kwargs["style"].lower() == "bright":
@@ -105,11 +142,11 @@ def printBlue(_str, **kwargs):
         **kwargs: are the key-value pair that print method takes besides string.
     """
     if not kwargs:
-        _print(f"""{colorama.Fore.BLUE}{_str}{colorama.Fore.RESET}""")
+        printk(f"""{colorama.Fore.BLUE}{_str}{colorama.Fore.RESET}""")
         return
 
     if not "style" in kwargs:
-        _print(f"""{colorama.Fore.BLUE}{_str}{colorama.Fore.RESET}""", **kwargs)
+        printk(f"""{colorama.Fore.BLUE}{_str}{colorama.Fore.RESET}""", **kwargs)
         return
 
     if kwargs["style"].lower() == "b" or kwargs["style"].lower() == "bright":
@@ -142,11 +179,11 @@ def printYellow(_str, **kwargs):
         **kwargs: are the key-value pair that print method takes besides string.
     """
     if not kwargs:
-        _print(f"""{colorama.Fore.YELLOW}{_str}{colorama.Fore.RESET}""")
+        printk(f"""{colorama.Fore.YELLOW}{_str}{colorama.Fore.RESET}""")
         return
 
     if not "style" in kwargs:
-        _print(f"""{colorama.Fore.YELLOW}{_str}{colorama.Fore.RESET}""", **kwargs)
+        printk(f"""{colorama.Fore.YELLOW}{_str}{colorama.Fore.RESET}""", **kwargs)
         return
 
     if kwargs["style"].lower() == "b" or kwargs["style"].lower() == "bright":
