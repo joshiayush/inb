3628c92b153c3d7550702c2e3e65314f5feb96da
Ayush Joshi <ayush854032@gmail.com>
2021-05-07 13:37:37 +0530

    fix (import): unable to import module Fernet
    import module Fernet properly as it is a part of the cryptography module
    
    new variable '__credentials_file'
    variable to store the credentials file location

--- src/creds/__init__.py
+++ src/creds/__init__.py
@@ -2,10 +2,12 @@ __name__ = "Credentials"
 __package__ = "Credentials"
 
 import os
-import Fernet
+from cryptography.fernet import Fernet
 
 __key = ""
 __key_file = "/Python/linkedin-bot/creds/.key.key"
+__credentials_file = "/Python/linkedin-bot/creds/credentialsFile.ini"
+
 
 if not os.path.exists(__key_file):
     __key = Fernet.generate_key()
