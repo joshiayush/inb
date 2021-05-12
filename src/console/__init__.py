7eaad382350b5caf903d710ad009b903c77e1f23
Ayush Joshi <ayush854032@gmail.com>
2021-05-12 11:39:41 +0530

    refactor (method): change methods to static methods
    static methods to enable or disable theme

--- src/console/__init__.py
+++ src/console/__init__.py
@@ -5,8 +5,10 @@ __package__ = "Console"
 class Theme(object):
     PARROT = True
 
+    @staticmethod
     def enable_theme_parrot() -> None:
         Theme.PARROT = True
 
+    @staticmethod
     def disable_theme_parrot() -> None:
         Theme.PARROT = False
