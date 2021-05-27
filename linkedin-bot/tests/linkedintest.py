a246e3435de6af2400c7ac14797c9699485373cb
Ayush Joshi <ayush854032@gmail.com>
2021-05-27 09:44:54 +0530

    test (WebElements): test for web elements
    test web element input tag for their presence in the login page if found then the test
    passed if not found then the test failed

--- linkedin-bot/tests/linkedintest.py
+++ linkedin-bot/tests/linkedintest.py
@@ -12,6 +12,7 @@ from linkedin.linkedin import LinkedIn
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 
 user_email = ''
 user_password = ''
@@ -27,11 +28,11 @@ with open("/Python/linkedin-bot/linkedin-bot/tests/creds/creds.json") as _file:
 
 def test_initial_state() -> None:
     assert LinkedIn.OLD_EMAIL == '', \
-        "LinkedInTest.py 'LinkedIn.OLD_EMAIL' should be emtpy initially! test failed " + failed
+        "tests/linkedintest.py '_linkedin.OLD_EMAIL' should be emtpy initially! Test failed " + failed
     assert LinkedIn.OLD_PASSWORD == '', \
-        "LinkedInTest.py 'LinkedIn.OLD_PASSWORD' should be empty initially! test failed " + failed
+        "tests/linkedintest.py '_linkedin.OLD_PASSWORD' should be empty initially! Test failed " + failed
     assert LinkedIn.SESSION_ALREADY_EXISTS == False, \
-        "LinkedInTest.py LinkedIn.SESSION_ALREADY_EXISTS' should be False initially! test failed " + failed
+        "tests/linkedintest.py '_linkedin.SESSION_ALREADY_EXISTS' should be False initially! Test failed " + failed
 
 
 def test_after_initialisation(_linkedin: LinkedIn) -> None:
@@ -39,35 +40,35 @@ def test_after_initialisation(_linkedin: LinkedIn) -> None:
     global user_password
 
     assert _linkedin.OLD_EMAIL == user_email, \
-        "LinkedInTest.py 'LinkedIn.OLD_EMAIL' should be equal to given user email! test failed " + failed
+        "tests/linkedintest.py '_linkedin.OLD_EMAIL' should be equal to given user email! Test failed " + failed
     assert _linkedin.OLD_PASSWORD == user_password, \
-        "LinkedInTest.py 'LinkedIn.OLD_PASSWORD' should be equal to given user password! test failed " + failed
+        "tests/linkedintest.py '_linkedin.OLD_PASSWORD' should be equal to given user password! Test failed " + failed
     assert _linkedin.SESSION_ALREADY_EXISTS == False, \
-        "LinkedInTest.py LinkedIn.SESSION_ALREADY_EXISTS' should be equal to True after initialisation! test failed " + failed
+        "tests/linkedintest.py _linkedin.SESSION_ALREADY_EXISTS' should be equal to True after initialisation! Test failed " + failed
 
 
 def test_getters(_linkedin: LinkedIn) -> None:
     assert isinstance(_linkedin.user_email, str), \
-        "LinkedInTest.py LinkedIn.user_email must return string type! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_email must return string type! Test failed " + failed
     assert isinstance(_linkedin.user_password, str), \
-        "LinkedInTest.py LinkedIn.user_password must return string type! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_password must return string type! Test failed " + failed
 
     global user_email
     global user_password
 
     assert _linkedin.user_email == user_email, \
-        "LinkedInTest.py LinkedIn.user_email must be equal to the given user email! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_email must be equal to the given user email! Test failed " + failed
     assert _linkedin.user_password == user_password, \
-        "LinkedInTest.py LinkedIn.user_password must be equal to the given user password! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_password must be equal to the given user password! Test failed " + failed
 
 
 def test_setters(_linkedin: LinkedIn) -> None:
     _linkedin.user_email = "xxx-xxx-xxx"
 
     assert isinstance(_linkedin.user_email, str), \
-        "LinkedInTest.py LinkedIn.user_email must set a string type! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_email must set a string type! Test failed " + failed
     assert _linkedin.user_email == "xxx-xxx-xxx", \
-        "LinkedInTest.py LinkedIn.user_email must set user email to the given user email! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_email must set user email to the given user email! Test failed " + failed
 
     global user_email
     _linkedin.user_email = user_email
@@ -75,9 +76,9 @@ def test_setters(_linkedin: LinkedIn) -> None:
     _linkedin.user_password = "xxx-xxx-xxx"
 
     assert isinstance(_linkedin.user_password, str), \
-        "LinkedInTest.py LinkedIn.user_password must set a string type! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_password must set a string type! Test failed " + failed
     assert _linkedin.user_password == "xxx-xxx-xxx", \
-        "LinkedInTest.py LinkedIn.user_password must set user password to the given user password! test failed " + failed
+        "tests/linkedintest.py _linkedin.user_password must set user password to the given user password! Test failed " + failed
 
     global user_password
     _linkedin.user_password = user_password
@@ -89,10 +90,10 @@ def test_get_chrome_driver_options_method(_linkedin: LinkedIn) -> None:
     _linkedin.set_ignore_certificate_error()
 
     assert isinstance(_linkedin.get_chrome_driver_options(), Options), \
-        "LinkedInTest.py LinkedIn.get_chrome_driver_options method must return list type! test failed " + failed
+        "tests/linkedintest.py _linkedin.get_chrome_driver_options method must return list type! Test failed " + failed
 
 
-def test_webdriver_chrome(_linkedin) -> None:
+def test_webdriver_chrome(_linkedin: LinkedIn) -> None:
     _linkedin.set_headless()
     _linkedin.set_browser_incognito_mode()
     _linkedin.set_ignore_certificate_error()
@@ -100,9 +101,30 @@ def test_webdriver_chrome(_linkedin) -> None:
     _linkedin.enable_webdriver_chrome(_linkedin.get_chrome_driver_options())
 
     assert isinstance(_linkedin.driver, Chrome), \
-        "LinkedInTest.py LinkedIn.driver must be of Chrome type! test failed " + failed
+        "tests/linkedintest.py _linkedin.driver must be of Chrome type! Test failed " + failed
 
     _linkedin.disable_webdriver_chrome()
+
+    LinkedIn.SESSION_ALREADY_EXISTS = False
+
+
+def test_web_elements(_linkedin: LinkedIn) -> None:
+    _linkedin.set_headless()
+    _linkedin.set_browser_incognito_mode()
+    _linkedin.set_ignore_certificate_error()
+
+    _linkedin.enable_webdriver_chrome(_linkedin.get_chrome_driver_options())
+
+    _linkedin.get_login_page("https://www.linkedin.com/login")
+
+    assert isinstance(_linkedin.get_email_box(), WebElement), \
+        "tests/linkedintest.py _linkedin.get_email_box method must return WebElement type! Test failed " + failed
+    assert isinstance(_linkedin.get_password_box(), WebElement), \
+        "tests/linkedintest.py _linkedin.get_password_box method must return WebElement type! Test failed " + failed
+
+    _linkedin.disable_webdriver_chrome()
+
+    LinkedIn.SESSION_ALREADY_EXISTS = False
 
 
 def test_linkedin() -> list:
@@ -113,7 +135,7 @@ def test_linkedin() -> list:
     try:
         test_initial_state()
         printGreen(
-            "LinkedInTest.py initial state test passed " + passed, style='b')
+            "tests/linkedintest.py initial state test passed " + passed, style='b')
         LinkedInTestSuccess += 1
     except AssertionError as error:
         printRed(error)
@@ -131,7 +153,7 @@ def test_linkedin() -> list:
     try:
         test_after_initialisation(_linkedin)
         printGreen(
-            "LinkedInTest.py after initial state test passed " + passed, style='b')
+            "tests/linkedintest.py after initial state test passed " + passed, style='b')
         LinkedInTestSuccess += 1
     except AssertionError as error:
         printRed(error)
@@ -142,7 +164,7 @@ def test_linkedin() -> list:
     try:
         test_getters(_linkedin)
         printGreen(
-            "LinkedInTest.py test getter methods passed " + passed, style='b')
+            "tests/linkedintest.py test getter methods passed " + passed, style='b')
         LinkedInTestSuccess += 1
     except AssertionError as error:
         printRed(error)
@@ -153,7 +175,7 @@ def test_linkedin() -> list:
     try:
         test_setters(_linkedin)
         printGreen(
-            "LinkedInTest.py test setter methods passed " + passed, style='b')
+            "tests/linkedintest.py test setter methods passed " + passed, style='b')
         LinkedInTestSuccess += 1
     except AssertionError as error:
         printRed(error)
@@ -164,7 +186,7 @@ def test_linkedin() -> list:
     try:
         test_get_chrome_driver_options_method(_linkedin)
         printGreen(
-            "LinkedInTest.py test for chrome driver options passed " + passed, style='b')
+            "tests/linkedintest.py test for chrome driver options passed " + passed, style='b')
         LinkedInTestSuccess += 1
     except AssertionError as error:
         printRed(error)
@@ -175,7 +197,18 @@ def test_linkedin() -> list:
     try:
         test_webdriver_chrome(_linkedin)
         printGreen(
-            "LinkedInTest.py test for chrome driver passed " + passed, style='b')
+            "tests/linkedintest.py test for chrome driver passed " + passed, style='b')
+        LinkedInTestSuccess += 1
+    except AssertionError as error:
+        printRed(error)
+        LinkedInTestFailed += 1
+    finally:
+        LinkedInTestNumber += 1
+
+    try:
+        test_web_elements(_linkedin)
+        printGreen(
+            "tests/linkedintest.py test for web elements passed " + passed, style='b')
         LinkedInTestSuccess += 1
     except AssertionError as error:
         printRed(error)
