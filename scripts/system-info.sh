def14644ef0f51ffb9b1a45cbf46027e89bf127e
Ayush Joshi <ayush854032@gmail.com>
2021-06-07 12:35:59 +0530

    style (indent): use tab width 2 for indentation
    use tab width 2 for indentation of shell script functions

--- scripts/system-info.sh
+++ scripts/system-info.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function getSystemInfo() {
-    # Function that finds the system platform Information.
-    case "$(/usr/bin/lsb_release -si)" in
+  # Function that finds the system platform Information.
+  case "$(/usr/bin/lsb_release -si)" in
     Ubuntu) echo "Ubuntu" ;;
     Mint) echo "Mint" ;;
     arch) echo "Arch" ;;
     *) echo "Can not get the System Information! Download Python manually" ;;
-    esac
+  esac
 }
