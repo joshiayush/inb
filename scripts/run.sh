ba998e25fcbcd8a6788db5ccea87073a79d6df52
Ayush Joshi <ayush854032@gmail.com>
2021-06-07 12:38:40 +0530

    fix (machine-type): tailor script for linux machines
    tailor script for linux machines because it's good to use only well tested code rather
    than putting some non-tested code into the script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,55 +1,15 @@
 #!/bin/bash
 
-function main() {
-    # Function main is the main function that starts the execution of the linkedin automator program it first checks if the
-    # requirements are present or not then it takes actions accordingly.
-    source /Python/linkedin-bot/scripts/execute.sh
-
-    if python3 --version | grep "Python*" >/dev/null; then
-        echo "Python is installed"
-        execute python3.7 "linux"
-    elif python2 --version | grep "Python*" >/dev/null; then
-        echo "Python is installed"
-        execute python2 "linux"
-    elif python --version | grep "Python*" >/dev/null; then
-        echo "Python is installed"
-        execute python "linux"
-    elif py --version | grep "Python*" >/dev/null; then
-        echo "Python is installed"
-        execute py "windows"
-    else
-        source /Python/linkedin-bot/scripts/python.sh
-        echo "Python is not installed"
-        installPython
-    fi
-}
-
-source /Python/linkedin-bot/scripts/garbage.sh
-
-# Starting the LinkedIn automation program if no command line arguments are passed otherwise handles the command line arguments.
-if [ $# -eq 0 ]; then
-    # Start if no arguments are passed in the command line and also delete the pycache if present.
-    main
-    if [ "$(checkIfGarbage)" = "yes" ]; then
-        deleteCache
-    fi
-else
-    # Getting the command line argument passed by the user
-    while getopts ":d:" opt; do
-        case $opt in
-        d)
-            if [ "$OPTARG" = "pycache" ]; then
-                if [ "$(checkIfGarbage)" = "yes" ]; then
-                    deleteCache
-                else
-                    echo "'__pycache__' does not exist!"
-                fi
-            fi
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            exit 1
-            ;;
-        esac
-    done
+if [ "$EUID" -eq 0 ]; then
+  echo "Run this script as normal user!"
+  exit
 fi
+
+if python3.7 --version | grep "Python*" >/dev/null; then
+  echo "python3.7 is installed, running program linkedin-bot/main.py ..."
+  python3.7 linkedin-bot/main.py
+else
+  echo "python3.7 is not installed, please install it first!"
+fi
+
+sudo ./scripts/delete-garbage.sh
