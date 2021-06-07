9a4e886783b5c8912f4f4e60a764c1ec20b0bc55
Ayush Joshi <ayush854032@gmail.com>
2021-06-07 12:27:49 +0530

    fix (stack): use stack only when it's neccessary
    remove function as the build can be done without using extra stack

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-function build() {
-    echo "Building linkedin-bot ..."
-    /usr/bin/python3.7 -m PyInstaller ./linkedin-bot/main.py
-    echo "Executing binary file main ..."
-    ./dist/main/main
-}   
+echo "Building linkedin-bot ..."
+/usr/bin/python3.7 -m PyInstaller ./linkedin-bot/main.py
 
-build
+echo "Executing binary file main ..."
+./dist/main/main   
