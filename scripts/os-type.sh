1b5edecc133cdfd06136ae6272a95950e10a90b8
Ayush Joshi <ayush854032@gmail.com>
2021-06-07 12:40:24 +0530

    refactor (rename): rename scripts for their new version
    rename scripts to give a much bolder outline of their functionality

--- scripts/os-type.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function getOsInfo() {
-    # Function that returns the name of the operating system that the user is running in its system, variable OSTYPE is a predefined
-    # variable that stores the name of the Operating System that is running currently.
-    echo $OSTYPE
-}
