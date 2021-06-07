78f1b14fac6f57ddbd3f1179e2254af95e80452c
Ayush Joshi <ayush854032@gmail.com>
2021-06-07 12:29:37 +0530

    fix (stack): use stack only when it's required
    don't use stack by creating functions every where this can be done without functions
    
    put the build directories and build files inside arrays and then delete them one by one

--- scripts/delete-builds.sh
+++ scripts/delete-builds.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-function deleteBuilds() {
-    echo "Deleting builds ..."
-    echo "Deleting build dir ..."
-    sudo rm -rf build
-    echo "Deleting dist dir ..."
-    sudo rm -rf dist
-    echo "Deleting main.spec file ..."
-    sudo rm main.spec
-}   
+if [ "$EUID" -ne 0 ]; then
+  echo "Root permissions required!"
+  exit
+fi
 
-deleteBuilds
+declare -a build_dirs=("build"
+  "dist")
+
+declare -a build_files=("main.spec")
+
+echo "Deleting builds ..."
+
+for dir in "${build_dirs[@]}"; do
+  echo "Deleting ${dir} directory ..."
+  sudo rm -rf dir
+done
+
+
+for file in "${build_files[@]}"; do
+  echo "Deleting ${file} file ..."
+  sudo rm file
+done
