6e34c120cb24c344a601f55cfd657d89b061f892
Ayush Joshi <ayush854032@gmail.com>
2021-06-25 17:20:34 +0530

    fix (mode): set filemode false before changing the permissions
    before changing the permissions you want to set the filemode to false so that git does
    not label them as modified

--- scripts/rwx.sh
+++ scripts/rwx.sh
@@ -5,9 +5,8 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-echo "Changing $PWD directory permissions ..."
-
-sudo chmod 777 -R $PWD
-
 echo "Setting git config core.filemode to false to not to track file bits when not in the main system ..."
 git config core.filemode false
+
+echo "Changing $PWD directory permissions ..."
+sudo chmod 777 -R $PWD
