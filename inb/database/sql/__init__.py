65ce47c1318875dda7b6a8c3366d22a1bb966829
Ayush Joshi <ayush854032@gmail.com>
2021-07-12 23:28:02 +0530

    fix (SQL): class SQL must not commit and close
    class SQL must not commit and close it should only return a connection variable that
    later can be used to close and commit changes

--- inb/database/sql/__init__.py
+++ inb/database/sql/__init__.py
@@ -5,21 +5,42 @@ import sqlite3
 
 from urllib.request import pathname2url
 
+from errors.error import DatabaseDoesNotExistException
+
+__all__ = ["SQL"]
+
 
 class SQL(object):
+    """Class SQL defines a prototype for the database api that we are going to use
+    in this project for performing CRUD operations.
+    """
 
-    def __init__(self: SQL, database: str = '', mode: str = '') -> None:
+    def __init__(self: SQL, database: str, mode: str) -> None:
+        """Constructor method opens a sqlite3 connection.
+
+        :Args:
+            - self: {SQL}
+            - database: {str} database path
+            - mode: {str} connection mode
+
+        :Raises:
+            - {DatabaseDoesNotExistException} if there is an OperationalError.
+        """
         try:
             self._connection = sqlite3.connect(
                 database="file:%s?mode=%s" % (pathname2url(database), mode), uri=True)
         except sqlite3.OperationalError as e:
-            raise e
-
-    def commit(self: SQL) -> None:
-        self._connection.commit()
-
-    def close(self: SQL) -> None:
-        self._connection.close()
+            raise DatabaseDoesNotExistException(
+                "Database %s does not exist to perform (%s) operations" % (database, mode))
 
     def connection(self: SQL) -> sqlite3.Connection:
+        """Method connection() returns the connection object formed during the initialisation
+        of SQL object.
+
+        :Args:
+            - self: {SQL}
+
+        :Returns:
+            - {sqlite3.Connection}
+        """
         return self._connection
