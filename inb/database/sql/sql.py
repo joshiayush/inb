27efb882efbe51610eca3abc8c15bdd970e7ae58
Ayush Joshi <ayush854032@gmail.com>
2021-07-12 23:25:57 +0530

    feat (index): add index variables to access values
    add index variables to access values from the table
    avoid spreading magic numbers around the source code

--- inb/database/sql/sql.py
+++ inb/database/sql/sql.py
@@ -3,13 +3,45 @@ from __future__ import annotations
 
 from typing import Any
 
+from rich.table import Table
+from rich.console import Console
+
 from . import SQL
+
+from errors.error import DatabaseDoesNotExistException
+
+__all__ = ["NAME_INDEX",
+           "EMAIL_INDEX",
+           "PASSWORD_INDEX",
+           "ENCRYPTION_KEY_INDEX",
+           "NAME_COLUMN",
+           "EMAIL_COLUMN",
+           "PASSWORD_COLUMN",
+           "ENCRYPTION_KEY_COLUMN",
+           "USERS_TABLE",
+           "Database"]
+
+NAME_INDEX: int = 0
+EMAIL_INDEX: int = 1
+PASSWORD_INDEX: int = 2
+ENCRYPTION_KEY_INDEX: int = 3
+
+NAME_COLUMN: str = "Name"
+EMAIL_COLUMN: str = "Email"
+PASSWORD_COLUMN: str = "Password"
+ENCRYPTION_KEY_COLUMN: str = "Key"
+
+USERS_TABLE = "Users"
 
 
 class Database(SQL):
+    """Class Database"""
 
     def __init__(self: Database, database: str, mode: str) -> None:
-        super(SQL, self).__init__(database=database, mode=mode)
+        try:
+            super(SQL, self).__init__(database=database, mode=mode)
+        except DatabaseDoesNotExistException as e:
+            raise e
 
     def create(self: Database, table: str, **kwargs):
         self.connection().cursor().execute(
@@ -22,14 +54,25 @@ class Database(SQL):
             f"""INSERT INTO {table} ({", ".join([f"{col}" for col in kwargs.keys()])}) 
             VALUES ({", ".join(["'%s'" % (val) for val in kwargs.values()])});""")
 
-    def read(self: Database, table: str, rows: str, *args) -> Any:
-        if rows == "*":
-            return self.connection().execute(
-                f"""SELECT {", ".join(args)} FROM sqlite_master WHERE type='table' AND NAME='{table}';""").fetchall()
+    def read(self: Database, *args: list, table: str, rows: str, where: str = None) -> Any:
+        if where == None or where.strip() == '':
+            if rows == "*":
+                return self.connection().execute(
+                    f"""SELECT {", ".join(args)} FROM sqlite_master WHERE type='table' AND NAME='{table}';""").fetchall()
 
-        if rows == ".":
-            return self.connection().execute(
-                f"""SELECT {", ".join(args)} FROM sqlite_master WHERE type='table' AND NAME='{table}';""").fetchone()
+            if rows == ".":
+                return self.connection().execute(
+                    f"""SELECT {", ".join(args)} FROM sqlite_master WHERE type='table' AND NAME='{table}';""").fetchone()
+        else:
+            if rows == "*":
+                return self.connection().execute(
+                    f"""SELECT {", ".join(args)} FROM sqlite_master WHERE type='table' AND NAME='{table}' AND {where};"""
+                ).fetchall()
+
+            if rows == ".":
+                return self.connection().execute(
+                    f"""SELECT {", ".join(args)} FROM sqlite_master WHERE type='table' AND NAME='{table}' AND {where};"""
+                ).fetchone()
 
     def update(self: Database, table: str, where: str, **kwargs):
         if not where:
@@ -51,3 +94,16 @@ class Database(SQL):
         else:
             self.connection().cursor().execute(
                 f"""DELETE FROM {table} WHERE {where};""")
+
+    def print(self: Database, table: str, rows: list) -> None:
+        _table = Table(title="%s Databse" % (table))
+
+        _table.add_column("Name", justify="center", no_wrap=True)
+        _table.add_column("Email", justify="center", no_wrap=True)
+        _table.add_column("Password", justify="center", no_wrap=True)
+        _table.add_column("Key", justify="center", no_wrap=True)
+
+        for _row in rows:
+            _table.add_row(*_row)
+
+        Console().print(table)
