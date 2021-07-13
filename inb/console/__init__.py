8aa84c446b4e619a96fd8b12b2cf8e967ddd762f
Ayush Joshi <ayush854032@gmail.com>
2021-07-13 23:24:53 +0530

    feat (colors): use hex colors for printing colored text
    use css-like hex color style this way rich will be able to choose from 16.7 million
    colors
    
    use single function to print output on the terminal, use conditionals inside it to
    select the color user must see the output in

--- inb/console/__init__.py
+++ inb/console/__init__.py
@@ -1,7 +1,18 @@
 """Package console provides all the functions to interact with the user using the terminal."""
-
 __name__ = "console"
 __package__ = "console"
+
+__all__ = ["RED_HEX",
+           "GREEN_HEX",
+           "BLUE_HEX",
+           "ORANGE_HEX",
+           "CYAN_HEX"]
+
+RED_HEX = "#FF0000"
+GREEN_HEX = "#00FF00"
+BLUE_HEX = "#0000FF"
+ORANGE_HEX = "#FF4200"
+CYAN_HEX = "#00FFFD"
 
 
 class Theme(object):
--- inb/console/print.py
+++ inb/console/print.py
@@ -1,148 +1,89 @@
 from typing import Any
+from typing import Optional
 
+from rich.style import Style
 from rich.console import Console
 
-
-def inbprint(object: Any,
-             bold: bool = False,
-             blink: bool = False,
-             blink2: bool = False,
-             concealed: bool = False,
-             italic: bool = False,
-             reverse: bool = False,
-             strike: bool = False,
-             underline: bool = False,
-             underline2: bool = False,
-             frame: bool = False,
-             encircle: bool = False,
-             overline: bool = False) -> None:
-    styles: list = []
-
-    def add(styles: list, style: str): return styles.append(
-        style) if eval(style) else None
-
-    add(styles=styles, style="bold")
-    add(styles=styles, style="blink")
-    add(styles=styles, style="blink2")
-    add(styles=styles, style="concealed")
-    add(styles=styles, style="italic")
-    add(styles=styles, style="revers")
-    add(styles=styles, style="strike")
-    add(styles=styles, style="underline")
-    add(styles=styles, style="underline2")
-    add(styles=styles, style="frame")
-    add(styles=styles, style="encircle")
-    add(styles=styles, style="overline")
-
-    Console().print(object, style=" ".join(styles))
+from console import RED_HEX
+from console import CYAN_HEX
+from console import BLUE_HEX
+from console import GREEN_HEX
+from console import ORANGE_HEX
 
 
-def inbprintred(object: Any,
-                bold: bool = False,
-                blink: bool = False,
-                blink2: bool = False,
-                concealed: bool = False,
-                italic: bool = False,
-                reverse: bool = False,
-                strike: bool = False,
-                underline: bool = False,
-                underline2: bool = False,
-                frame: bool = False,
-                encircle: bool = False,
-                overline: bool = False) -> None:
-    styles: list = []
+def inbprint(obj: Any,
+             color: Optional[str] = None,
+             link: Optional[str] = None,
+             bold: Optional[bool] = False,
+             blink: Optional[bool] = False,
+             blink2: Optional[bool] = False,
+             conceal: Optional[bool] = False,
+             italic: Optional[bool] = False,
+             reverse: Optional[bool] = False,
+             strike: Optional[bool] = False,
+             underline: Optional[bool] = False,
+             underline2: Optional[bool] = False,
+             frame: Optional[bool] = False,
+             encircle: Optional[bool] = False,
+             overline: Optional[bool] = False) -> None:
+    """Function inbprint() prints the given object out to the console using the rich
+    library methods to print formatted text on the screen.
 
-    def add(styles: list, style: str): return styles.append(
-        style) if eval(style) else None
+    :Args:
+        - obj: {Any} object to print on the terminal
+        - color: {Optional[str]} color of the text
+        - link: {Optional[str]} if the object shows a link
+        - bold: {Optional[str]} print bold text
+        - blink: {Optional[str]} print blinking text
+        - blink2: {Optional[str]} print fast blinking text
+        - conceal: {Optional[str]} print concealed text
+        - italic: {Optional[str]} print italic text
+        - reverse: {Optional[str]} print reverse text
+        - strike: {Optional[str]} add strike through the text
+        - underline: {Optional[str]} add underline on the text 
+        - underline2: {Optional[str]} add double underline on the text
+        - frame: {Optional[str]} for framed text
+        - encircle: {Optional[str]} for encircled text
+        - overline: {Optional[str]} for overlined text
 
-    add(styles=styles, style="bold")
-    add(styles=styles, style="blink")
-    add(styles=styles, style="blink2")
-    add(styles=styles, style="concealed")
-    add(styles=styles, style="italic")
-    add(styles=styles, style="revers")
-    add(styles=styles, style="strike")
-    add(styles=styles, style="underline")
-    add(styles=styles, style="underline2")
-    add(styles=styles, style="frame")
-    add(styles=styles, style="encircle")
-    add(styles=styles, style="overline")
+    :Returns:
+        - {None}
+    """
+    def style() -> Style:
+        """Function style() creates a Style object for the rich's print method.
 
-    if len(styles) == 0:
-        Console().print(object, style="red")
-    else:
-        Console().print(object, style=f"""{" ".join(styles)} on red""")
+        :Returns:
+            - {Style}
+        """
+        _kwargs = {link: link,
+                   bold: bold,
+                   blink: blink,
+                   blink2: blink2,
+                   conceal: conceal,
+                   italic: italic,
+                   reverse: reverse,
+                   strike: strike,
+                   underline: underline,
+                   underline2: underline2,
+                   frame: frame,
+                   encircle: encircle,
+                   overline: overline}
 
+        if color.lower() == 'r' or color.lower() == "red":
+            return Style(color=RED_HEX, **_kwargs)
 
-def inbprintgreen(object: Any,
-                  bold: bool = False,
-                  blink: bool = False,
-                  blink2: bool = False,
-                  concealed: bool = False,
-                  italic: bool = False,
-                  reverse: bool = False,
-                  strike: bool = False,
-                  underline: bool = False,
-                  underline2: bool = False,
-                  frame: bool = False,
-                  encircle: bool = False,
-                  overline: bool = False) -> None:
-    styles: list = []
+        if color.lower() == 'g' or color.lower() == "green":
+            return Style(color=GREEN_HEX, **_kwargs)
 
-    def add(styles: list, style: str): return styles.append(
-        style) if eval(style) else None
+        if color.lower() == 'b' or color.lower() == "blue":
+            return Style(color=BLUE_HEX, **_kwargs)
 
-    add(styles=styles, style="bold")
-    add(styles=styles, style="blink")
-    add(styles=styles, style="blink2")
-    add(styles=styles, style="concealed")
-    add(styles=styles, style="italic")
-    add(styles=styles, style="revers")
-    add(styles=styles, style="strike")
-    add(styles=styles, style="underline")
-    add(styles=styles, style="underline2")
-    add(styles=styles, style="frame")
-    add(styles=styles, style="encircle")
-    add(styles=styles, style="overline")
+        if color.lower() == 'c' or color.lower() == "cyan":
+            return Style(color=CYAN_HEX, **_kwargs)
 
-    if len(styles) == 0:
-        Console().print(object, style="green")
-    else:
-        Console().print(object, style=f"""{" ".join(styles)} on green""")
+        if color.lower() == 'o' or color.lower() == "orange":
+            return Style(color=ORANGE_HEX, **_kwargs)
 
+        return Style(**_kwargs)
 
-def inbprintblue(object: Any,
-                 bold: bool = False,
-                 blink: bool = False,
-                 blink2: bool = False,
-                 concealed: bool = False,
-                 italic: bool = False,
-                 reverse: bool = False,
-                 strike: bool = False,
-                 underline: bool = False,
-                 underline2: bool = False,
-                 frame: bool = False,
-                 encircle: bool = False,
-                 overline: bool = False) -> None:
-    styles: list = []
-
-    def add(styles: list, style: str): return styles.append(
-        style) if eval(style) else None
-
-    add(styles=styles, style="bold")
-    add(styles=styles, style="blink")
-    add(styles=styles, style="blink2")
-    add(styles=styles, style="concealed")
-    add(styles=styles, style="italic")
-    add(styles=styles, style="revers")
-    add(styles=styles, style="strike")
-    add(styles=styles, style="underline")
-    add(styles=styles, style="underline2")
-    add(styles=styles, style="frame")
-    add(styles=styles, style="encircle")
-    add(styles=styles, style="overline")
-
-    if len(styles) == 0:
-        Console().print(object, style="blue")
-    else:
-        Console().print(object, style=f"""{" ".join(styles)} on blue""")
+    Console().print(obj, style=style())
