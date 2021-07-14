eba5729d204792f4a14ecaaee6bb1d92b3d1e7b8
Ayush Joshi <ayush854032@gmail.com>
2021-07-14 12:46:42 +0530

    feat (color): add default color
    add default terminal color

--- inb/console/__init__.py
+++ inb/console/__init__.py
@@ -6,10 +6,12 @@ __all__ = ["RED_HEX",
            "GREEN_HEX",
            "BLUE_HEX",
            "ORANGE_HEX",
-           "CYAN_HEX"]
+           "CYAN_HEX",
+           "DEFAULT"]
 
 RED_HEX = "#FF0000"
 GREEN_HEX = "#00FF00"
 BLUE_HEX = "#0000FF"
 ORANGE_HEX = "#FF4200"
 CYAN_HEX = "#00FFFD"
+DEFAULT = "default"
