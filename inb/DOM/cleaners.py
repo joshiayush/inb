a6a06899a1ad01a5e437d5e07c77fc550bb149de
Ayush Joshi <ayush854032@gmail.com>
2021-07-15 14:54:10 +0530

    fix (loop): break loop once successful
    break the loop after setting the display of the message overlay to none

--- inb/DOM/cleaners.py
+++ inb/DOM/cleaners.py
@@ -1,5 +1,4 @@
 from errors.error import PropertyNotExistException
-from errors.error import FailedLoadingResourceException
 
 from selenium.webdriver.common.by import By
 
@@ -10,7 +9,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
 
-def clear_msg_overlay(self: object) -> None:
+def ClearMessageOverlay(self: object) -> None:
     """Function clear_msg_overlay() clears the message overlay that gets on the top of the
     network page.
 
@@ -19,8 +18,6 @@ def clear_msg_overlay(self: object) -> None:
 
     :Raises:
         - {PropertyNotExistException} if object 'self' does not has a property called 'driver'.
-        - {NoSuchElementException} if there is no such element with the given css selector.
-        - {FailedLoadingResourceException} if there is a 'TimeoutException' from 'WebDriverWait'.
 
     :Returns:
         - {None}
@@ -29,17 +26,17 @@ def clear_msg_overlay(self: object) -> None:
         raise PropertyNotExistException(
             "Object 'self' must have a property 'driver' in it!")
 
-    try:
-        WebDriverWait(getattr(self, "driver"), 10).until(
-            expected_conditions.presence_of_element_located(
-                (By.CSS_SELECTOR, "div[class^='msg-overlay-list-bubble']")))
-        
-        getattr(getattr(self, "driver"), "execute_script")(("""
-        (function () {
-          document.querySelector("div[class^='msg-overlay-list-bubble']").style = "display: none";
-        })();"""))
-    except NoSuchElementException:
-        raise NoSuchElementException(
-            "There is not such element exist by css selector 'div[class^='msg-overlay-list-bubble']'!")
-    except TimeoutException:
-        raise FailedLoadingResourceException("ERR_LOADING_RESOURCE")
+    while True:
+        try:
+            WebDriverWait(getattr(self, "driver"), 10).until(
+                expected_conditions.presence_of_element_located(
+                    (By.CSS_SELECTOR, "div[class^='msg-overlay-list-bubble']")))
+
+            getattr(getattr(self, "driver"), "execute_script")(
+                """document.querySelector("div[class^='msg-overlay-list-bubble']").style = "display: none";""")
+
+            break
+        except NoSuchElementException:
+            return
+        except TimeoutException:
+            continue
