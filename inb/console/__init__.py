6fed5a4cd3820c49473e94351c44ca5d3b64cf26
Ayush Joshi <ayush854032@gmail.com>
2021-07-16 12:36:12 +0530

    styles (colors): yellow and light green color
    yellow and light green colors

--- inb/console/__init__.py
+++ inb/console/__init__.py
@@ -5,13 +5,18 @@ __package__ = "console"
 __all__ = ["RED_HEX",
            "GREEN_HEX",
            "BLUE_HEX",
+           "YELLOW_HEX"
            "ORANGE_HEX",
            "CYAN_HEX",
-           "DEFAULT"]
+           "DEFAULT",
+           "LIGHT_GREEN_HEX"]
 
 RED_HEX = "#FF0000"
 GREEN_HEX = "#00FF00"
 BLUE_HEX = "#0000FF"
+YELLOW_HEX = "#fffb00"
 ORANGE_HEX = "#FF4200"
 CYAN_HEX = "#00FFFD"
 DEFAULT = "default"
+
+LIGHT_GREEN_HEX = "#66ff33"
