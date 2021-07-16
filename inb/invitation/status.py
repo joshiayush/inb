b5552958ec11f5632339d2f69feb66734bb6ef94
Ayush Joshi <ayush854032@gmail.com>
2021-07-16 12:35:35 +0530

    feat (styles): style output
    style output using rich colors

--- inb/invitation/status.py
+++ inb/invitation/status.py
@@ -9,12 +9,26 @@ from . import SENT_STATUS_SYMBOL
 from . import FAILED_STATUS_SYMBOL
 from . import UNDEFINED_STATUS_SYMBOL
 
+from console import RED_HEX
+from console import CYAN_HEX
+from console import GREEN_HEX
+from console import YELLOW_HEX
+from console import LIGHT_GREEN_HEX
+
 from console.print import inbprint
 
 
 class Invitation(object):
 
-    def __init__(self: Invitation, name: str, occupation: str, status: str = '', elapsed_time: int = 0) -> None:
+    SLEEP_TIME_AFTER_LOGGING = 0.18
+
+    def __init__(
+        self: Invitation,
+        name: str,
+        occupation: str,
+        status: str = '',
+        elapsed_time: int = 0
+    ) -> None:
         self._name = name
 
         if len(occupation) >= 50:
@@ -26,19 +40,19 @@ class Invitation(object):
         self._failure_rate = 0
 
         if status == "sent":
-            self._status = SENT_STATUS_SYMBOL
+            self._status = f"[bold {GREEN_HEX}]" + SENT_STATUS_SYMBOL
 
             global SUCCESS_RATE
             SUCCESS_RATE += 1
             self._success_rate = SUCCESS_RATE
         elif status == "failed":
-            self._status = FAILED_STATUS_SYMBOL
+            self._status = f"[bold {RED_HEX}]" + FAILED_STATUS_SYMBOL
 
             global FAILURE_RATE
             FAILURE_RATE += 1
             self._failure_rate = FAILURE_RATE
         else:
-            self._status = UNDEFINED_STATUS_SYMBOL
+            self._status = f"[bold {CYAN_HEX}]" + UNDEFINED_STATUS_SYMBOL
 
         try:
             self._elapsed_time = str(elapsed_time)[0:5] + "s"
@@ -55,9 +69,9 @@ class Invitation(object):
         self.clears_creen()
 
         inbprint()
-        inbprint(f"""{self._status} {self._name}\n\n"""
-                 f"""{self._occupation}\n\n"""
-                 f"""Success: {self._success_rate} Failed: {self._failure_rate} Elapsed: {self._elapsed_time}""")
+        inbprint(f"""{self._status} [bold {YELLOW_HEX}]{self._name}\n\n"""
+                 f"""[bold {LIGHT_GREEN_HEX}]{self._occupation}\n\n"""
+                 f"""[bold blink {GREEN_HEX}]Success: {self._success_rate} [bold blink {RED_HEX}]Failed: {self._failure_rate} [bold blink {CYAN_HEX}]Elapsed: {self._elapsed_time}""")
         inbprint()
 
-        time.sleep(0.18)
+        time.sleep(self.SLEEP_TIME_AFTER_LOGGING)
