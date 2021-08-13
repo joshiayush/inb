0fdf712adb581b7836e33e486c61244715134343
Ayush Joshi <ayush854032@gmail.com>
2021-08-13 10:57:37 +0530

    refactor (parser): inline descriptions and help
    no need to separate the description and help string for the commands, we
    don't need it once we have used it in the parser module

--- inb/inb.py
+++ inb/inb.py
@@ -6,22 +6,8 @@ from linkedin import __version__
 
 from lib.handler import CommandHandler
 
+from helpers.figlet import CreateFigletString
 from helpers.parser.parser import CreateParser
-
-from helpers.command import get_send_help
-from helpers.command import get_show_help
-from helpers.command import get_config_help
-from helpers.command import get_delete_help
-from helpers.command import get_developer_help
-from helpers.command import get_invitation_limit_help
-
-from helpers.command import get_lbot_description
-from helpers.command import get_send_description
-from helpers.command import get_show_description
-from helpers.command import get_config_description
-from helpers.command import get_delete_description
-from helpers.command import get_developer_description
-from helpers.command import get_invitation_limit_description
 
 
 # usage: lbot [-h] {send,config,show,delete,developer} ...
@@ -50,10 +36,16 @@ from helpers.command import get_invitation_limit_description
 #   -h, --help            show this help message and exit
 
 
-parser = CreateParser(
-    prog="inb", description=get_lbot_description(), formatter_class=RawDescriptionHelpFormatter)
+parser = CreateParser(prog="inb",
+                      description=(
+                          f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                          f"""LinkedIn Bash, version {__version__}(1)-release (lbot-{__version__})\n"""
+                          """These commands are defined internally. Type '--help' to see this list\n"""
+                          """Type (command) --help to know more about that command"""),
+                      formatter_class=RawDescriptionHelpFormatter)
 
-subparsers = parser.add_subparsers(help="available actions", metavar=None)
+subparsers = parser.add_subparsers(help="available actions",
+                                   metavar=None)
 
 
 # Usage: inb send [-h] [-e [EMAIL]] [-p [PASSWORD]] [-c] [-i] [-ngpu] [-m]
@@ -84,13 +76,23 @@ subparsers = parser.add_subparsers(help="available actions", metavar=None)
 #   -m, --start-maximized
 #                         set browser in full screen
 
-send = subparsers.add_parser(
-    "send", description=get_send_description(), formatter_class=RawDescriptionHelpFormatter, help=get_send_help())
+send = subparsers.add_parser("send",
+                             description=(
+                                 f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                                 """Command 'send' sends invitation to people on linkedin."""),
+                             formatter_class=RawDescriptionHelpFormatter,
+                             help=("""Command 'send' sends invitation to people on linkedin."""))
 
-send.add_argument(
-    "-e", "--email", type=str, nargs="?", default=None, help="User's email address")
-send.add_argument(
-    "-p", "--password", type=str, nargs="?", default=None, help="User's password")
+send.add_argument("-e", "--email",
+                  type=str,
+                  nargs="?",
+                  default=None,
+                  help="User's email address")
+send.add_argument("-p", "--password",
+                  type=str,
+                  nargs="?",
+                  default=None,
+                  help="User's password")
 
 # usage: lbot send limit [-h] [limit]
 #
@@ -112,29 +114,47 @@ send.add_argument(
 #   -h, --help  show this help message and exit
 
 
-limit_subparsers = send.add_subparsers(
-    help="available actions", metavar=None)
+limit_subparsers = send.add_subparsers(help="available actions",
+                                       metavar=None)
 
-limit = limit_subparsers.add_parser(
-    "limit",  description=get_invitation_limit_description(), formatter_class=RawDescriptionHelpFormatter,
-    help=get_invitation_limit_help())
+limit = limit_subparsers.add_parser("limit",
+                                    description=(
+                                        f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                                        """Flag 'limit' is used to set the daily invitation limit\n"""
+                                        """Limit must not exceed by 80 otherwise you'll be blocked for a entire\n"""
+                                        """week"""),
+                                    formatter_class=RawDescriptionHelpFormatter,
+                                    help=("""Flag 'limit' sets the daily invitation limit"""))
 
-limit.add_argument("limit", type=int, nargs="?", default=20)
+limit.add_argument("limit",
+                   type=int,
+                   nargs="?",
+                   default=20)
 
 limit.set_defaults(limit=20)
 
 
-send.add_argument(
-    "-c", "--cookies", action="store_true", help="uses cookies for authentication")
-send.add_argument(
-    "-i", "--incognito", action="store_true", help="set browser in incognito mode")
-send.add_argument(
-    "-ngpu", "--headless", action="store_true", help="starts chrome in headless mode")
-send.add_argument(
-    "-m", "--start-maximized", action="store_true", help="set browser in full screen")
+send.add_argument("-c", "--cookies",
+                  action="store_true",
+                  help="uses cookies for authentication")
+send.add_argument("-i", "--incognito",
+                  action="store_true",
+                  help="set browser in incognito mode")
+send.add_argument("-ngpu", "--headless",
+                  action="store_true",
+                  help="starts chrome in headless mode")
+send.add_argument("-m", "--start-maximized",
+                  action="store_true",
+                  help="set browser in full screen")
 
-send.set_defaults(
-    which="send", email=None, password=None, headless=False, limit=20, cookies=False, incognito=False, start_maximized=False)
+send.set_defaults(which="send",
+                  email=None,
+                  password=None,
+                  headless=False,
+                  limit=20,
+                  cookies=False,
+                  incognito=False,
+                  start_maximized=False)
 
 
 # usage: inb config [-h] [EMAIL] [PASSWORD]
@@ -159,15 +179,30 @@ send.set_defaults(
 #   -h, --help  show this help message and exit
 
 
-config = subparsers.add_parser(
-    "config", description=get_config_description(), formatter_class=RawDescriptionHelpFormatter, help=get_config_help())
+config = subparsers.add_parser("config",
+                               description=(
+                                   f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                                   """Command 'config' is used to add user's credentials to the database\n"""
+                                   """Adding user's credentials to the database for ever or until user deletes\n"""
+                                   """them makes it feasible for user to send invitations without entering the\n"""
+                                   """fields again and again"""),
+                               formatter_class=RawDescriptionHelpFormatter,
+                               help=("""Command 'config' is used to store user's credentials"""))
 
-config.add_argument(
-    "EMAIL", type=str, nargs="?", default=None, help="user's email address")
-config.add_argument(
-    "PASSWORD", type=str, nargs="?", default=None, help="user's password")
+config.add_argument("EMAIL",
+                    type=str,
+                    nargs="?",
+                    default=None,
+                    help="user's email address")
+config.add_argument("PASSWORD",
+                    type=str,
+                    nargs="?",
+                    default=None,
+                    help="user's password")
 
-config.set_defaults(which="config", EMAIL=None, PASSWORD=None)
+config.set_defaults(which="config",
+                    EMAIL=None,
+                    PASSWORD=None)
 
 
 # usage: lbot show [-h] [-e] [-p] [keyword]
@@ -191,19 +226,33 @@ config.set_defaults(which="config", EMAIL=None, PASSWORD=None)
 #   -p, --password  print user's password
 
 
-show = subparsers.add_parser(
-    "show", description=get_show_description(), formatter_class=RawDescriptionHelpFormatter, help=get_show_help())
+show = subparsers.add_parser("show",
+                             description=(
+                                 f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                                 """Command 'show' prints the information stored in the database\n"""
+                                 """For example -> email, password ..."""),
+                             formatter_class=RawDescriptionHelpFormatter,
+                             help=("""Command 'show' prints the information that is in the database"""))
 
-show.add_argument("keyword", type=str, nargs="?", default=None)
+show.add_argument("keyword",
+                  type=str,
+                  nargs="?",
+                  default=None)
 
-show.add_argument(
-    "-e", "--email", action="store_true", help="print user's email address")
-show.add_argument(
-    "-p", "--password", action="store_true", help="print user's password")
-show.add_argument(
-    "-d", "--decrypt", action="store_true", help="print information in decrypt form")
+show.add_argument("-e", "--email",
+                  action="store_true",
+                  help="print user's email address")
+show.add_argument("-p", "--password",
+                  action="store_true",
+                  help="print user's password")
+show.add_argument("-d", "--decrypt",
+                  action="store_true",
+                  help="print information in decrypt form")
 
-show.set_defaults(which="show", keyword=None, email=None, password=None)
+show.set_defaults(which="show",
+                  keyword=None,
+                  email=None,
+                  password=None)
 
 
 # usage: lbot delete [-h] keyword
@@ -225,12 +274,19 @@ show.set_defaults(which="show", keyword=None, email=None, password=None)
 #   -h, --help  show this help message and exit
 
 
-delete = subparsers.add_parser(
-    "delete", description=get_delete_description(), formatter_class=RawDescriptionHelpFormatter, help=get_delete_help())
+delete = subparsers.add_parser("delete",
+                               description=(
+                                   f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                                   """Command 'delete' deletes the information stored in the database\n"""
+                                   """'delete' deletes information like 'key', 'cookies' ..."""),
+                               formatter_class=RawDescriptionHelpFormatter,
+                               help=("""Command 'delete' deletes the information stored in the database"""))
 
-delete.add_argument("keyword", type=str)
+delete.add_argument("keyword",
+                    type=str)
 
-delete.set_defaults(which="delete", keyword=None)
+delete.set_defaults(which="delete",
+                    keyword=None)
 
 
 # usage: lbot developer [-h] [-n] [-l] [-g] [-m] [-e]
@@ -253,23 +309,35 @@ delete.set_defaults(which="delete", keyword=None)
 #   -e, --email     print developer email address
 
 
-developer = subparsers.add_parser(
-    "developer", description=get_developer_description(), formatter_class=RawDescriptionHelpFormatter,
-    help=get_developer_help())
+developer = subparsers.add_parser("developer",
+                                  description=(
+                                      f"""{CreateFigletString("LinkedIn Bot")}\n"""
+                                      """Command 'developer' prints the information about the author"""),
+                                  formatter_class=RawDescriptionHelpFormatter,
+                                  help=("""Command 'developer' prints the information about the author"""))
 
-developer.add_argument(
-    "-n", "--name", action="store_true", help="print developer name")
-developer.add_argument(
-    "-l", "--linkedin", action="store_true", help="print developer linkedin")
-developer.add_argument(
-    "-g", "--github", action="store_true", help="print developer github")
-developer.add_argument(
-    "-m", "--mobile", action="store_true", help="print developer mobile number")
-developer.add_argument(
-    "-e", "--email", action="store_true", help="print developer email address")
+developer.add_argument("-n", "--name",
+                       action="store_true",
+                       help="print developer name")
+developer.add_argument("-l", "--linkedin",
+                       action="store_true",
+                       help="print developer linkedin")
+developer.add_argument("-g", "--github",
+                       action="store_true",
+                       help="print developer github")
+developer.add_argument("-m", "--mobile",
+                       action="store_true",
+                       help="print developer mobile number")
+developer.add_argument("-e", "--email",
+                       action="store_true",
+                       help="print developer email address")
 
-developer.set_defaults(
-    which="developer", name=False, linkedin=False, github=False, mobile=False, email=False)
+developer.set_defaults(which="developer",
+                       name=False,
+                       linkedin=False,
+                       github=False,
+                       mobile=False,
+                       email=False)
 
 if len(sys.argv) <= 1:
     exit()
