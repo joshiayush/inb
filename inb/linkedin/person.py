3ec26f4148b3ce03e6b83fb5241bae9daca14cb2
Ayush Joshi <ayush854032@gmail.com>
2021-08-15 15:47:41 +0530

    style (person): style change in function transform_to_object
    style change in transform_to_object function's code

--- inb/linkedin/person.py
+++ inb/linkedin/person.py
@@ -41,7 +41,7 @@ from .DOM.javascript import JS
 
 
 class Person_Info(object):
-    base_linkedin_url: str = "https://www.linkedin.com"
+    BASE_LINKEDIN_URL: str = "https://www.linkedin.com"
 
     def __init__(
             self: Person_Info,
@@ -49,16 +49,17 @@ class Person_Info(object):
             occupation: Union[str, None] = None,
             profile_url: Union[str, None] = None,
             photo_url: Union[str, None] = None,
-            connect_button: Union[str, None] = None
+            connect_button: Union[webdriver.Chrome, None] = None
     ) -> None:
         self._name = name
         self._occupation = occupation
         self._profile_url = profile_url
         self._photo_url = photo_url
         self._connect_button = connect_button
+        self._person_id = self.person_id()
 
     def person_id(self: Person_Info) -> str:
-        _url_base: str = self.base_linkedin_url + "/in/"
+        _url_base: str = self.BASE_LINKEDIN_URL + "/in/"
         _indx: int = self._profile_url.find(_url_base) + len(_url_base)
 
         _profile_url: str = self._profile_url
@@ -84,7 +85,7 @@ class Person_Info(object):
 
 
 class Person(object):
-    def __init__(self, driver: webdriver.Chrome) -> None:
+    def __init__(self: Person, driver: webdriver.Chrome) -> None:
         if not isinstance(driver, webdriver.Chrome):
             raise Exception("'%(driver_type)s' object is not a 'webdriver' object" % {
                             "driver_type": type(driver)})
@@ -116,8 +117,8 @@ class Person(object):
             nonlocal self
 
             _xpath: str = Path_To_Element_By.SUGGESTION_BOX_ELEMENT_XPATH
-            _xpath = _xpath[:-3] + \
-                '[' + str(self.__suggestion_box_element_count + 1) + ']'
+            _xpath = _xpath[:-3] + '[' + \
+                str(self.__suggestion_box_element_count + 1) + ']'
 
             self.__suggestion_box_element_count += 1
             self.__load_page()
@@ -133,45 +134,34 @@ class Person(object):
                     continue
 
         def transform_to_object(li: webdriver.Chrome) -> Person_Info:
-            _info_container: webdriver.Chrome = \
-                li.find_element_by_css_selector(
-                    "div[class='discover-entity-type-card__info-container']")
+            _info_container: webdriver.Chrome = li.find_element_by_css_selector(
+                "div[class='discover-entity-type-card__info-container']")
+            _anchor_tag: webdriver.Chrome = _info_container.find_element_by_tag_name(
+                "a")
+            _img_tag: webdriver.Chrome = _anchor_tag.find_element_by_tag_name(
+                "img")
+            _bottom_container: webdriver.Chrome = li.find_element_by_css_selector(
+                "div[class^='discover-entity-type-card__bottom-container']")
+            _footer: webdriver.Chrome = _bottom_container.find_element_by_tag_name(
+                "footer")
 
-            _anchor_tag: webdriver.Chrome = \
-                _info_container.find_element_by_tag_name("a")
-
-            _person_profile_url: str = \
-                Person_Info.base_linkedin_url + \
-                _anchor_tag.get_attribute("href")
-
-            _img_tag: webdriver.Chrome = \
-                _anchor_tag.find_element_by_tag_name("img")
-
+            _person_name: str = _anchor_tag.find_element_by_css_selector(
+                "span[class^='discover-person-card__name']").text
+            _person_occupation: str = _anchor_tag.find_element_by_css_selector(
+                "span[class^='discover-person-card__occupation']").text
             _person_photo_url: str = _img_tag.get_attribute("src")
+            _person_profile_url: str = "%(base_url)s%(profile_path)s" % {
+                "base_url": Person_Info.BASE_LINKEDIN_URL,
+                "profile_path": _anchor_tag.get_attribute("href")}
+            _person_connect_button: str = _footer.find_element_by_css_selector(
+                f"button[aria-label^='Invite']")
 
-            _person_name: str = \
-                _anchor_tag.find_element_by_css_selector(
-                    "span[class^='discover-person-card__name']").text
-
-            _person_occupation: str = \
-                _anchor_tag.find_element_by_css_selector(
-                    "span[class^='discover-person-card__occupation']").text
-
-            _bottom_container: webdriver.Chrome = \
-                li.find_element_by_css_selector(
-                    "div[class^='discover-entity-type-card__bottom-container']")
-
-            _footer: webdriver.Chrome = \
-                _bottom_container.find_element_by_tag_name("footer")
-
-            _person_connect_button: str = \
-                _footer.find_element_by_css_selector(
-                    f"button[aria-label^='Invite']")
-
-            return Person_Info(name=_person_name,
-                               occupation=_person_occupation,
-                               profile_url=_person_profile_url,
-                               photo_url=_person_photo_url,
-                               connect_button=_person_connect_button)
+            return Person_Info(
+                name=_person_name,
+                occupation=_person_occupation,
+                profile_url=_person_profile_url,
+                photo_url=_person_photo_url,
+                connect_button=_person_connect_button
+            )
 
         return transform_to_object(get_suggestion_box_person_li())
