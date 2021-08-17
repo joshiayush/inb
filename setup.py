70f97f33f95773635a1b3ea917946106d100beb8
Ayush Joshi <ayush854032@gmail.com>
2021-08-17 11:07:53 +0530

    chore (setup): update setup.py
    udpate setup.py file

--- setup.py
+++ setup.py
@@ -1,34 +1,9 @@
-import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.md")) as file:
-    long_description = file.read()
-
-build_files = []
-
-for root, directories, files in os.walk(os.getcwd()):
-    for file in files:
-        if file.endswith(".py"):
-            build_files.append(os.path.join(root, file)[:-3])
-
-build_files.remove("/Python/linkedin-bot/setup")
-
-setup(
-    name="linkedin-bot",
-    version="1.22.7",
-    url="https://github.com/JoshiAyush/linkedin-bot",
-    description="A tool to automate everything on LinkedIn",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    py_modules=build_files,
-    author="Ayush Joshi",
-    author_email="ayush854032@gmail.com",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: Public Domain",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules :: Automation :: Selenium :: Webdriver :: Chromedirver",
-    ]
-)
+setup(name="inb",
+      version="1.51.35",
+      description="inb is a command line tool to automate LinkedIn. Achieve 500+ connections very easily you just need to try it.",
+      url="https://github.com/JoshiAyush/inb",
+      license="MIT",
+      author="Ayush Joshi",
+      author_email="ayush854032@gmail.com")
