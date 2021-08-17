342f9f4d8a2340895f3a30bcbb3dad2aa4649eb2
Ayush Joshi <ayush854032@gmail.com>
2021-08-17 10:10:31 +0530

    docs (helpers): document helpers
    add MIT license to helpers module
    
    this commit also brings a fix in the Window::setcursorposition method
    check if file is None or not

--- inb/helpers/window.py
+++ inb/helpers/window.py
@@ -1,3 +1,26 @@
+# MIT License
+#
+# Copyright (c) 2019 Creative Commons
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""from __future__ imports must occur at the beginning of the file. DO NOT CHANGE!"""
 from __future__ import annotations
 
 import re
@@ -5,11 +28,11 @@ import sys
 import tty
 import termios
 
-from console.print import inbprint
+from typing import TextIO
 
 
 class Terminal(object):
-    def setcursorposition(self: Terminal, x: int, y: int) -> None:
+    def setcursorposition(self: Terminal, x: int, y: int, file: TextIO = sys.stdout) -> None:
         """Method setcursorposition() sets the console cursor position.
 
         :Args:
@@ -20,7 +43,8 @@ class Terminal(object):
         :Returns:
             - {None}
         """
-        inbprint("%c[%d;%df" % (0x1B, y, x), end='')
+        if not file == None:
+            file.write("%c[%d;%df" % (0x1B, y, x))
 
     def getcursorposition(self: Terminal) -> tuple:
         """Method getcursorposition() returns row and the column number at which the cursor is
@@ -44,7 +68,7 @@ class Terminal(object):
             while True:
                 _buffer += sys.stdin.read(1)
 
-                if _buffer[-1] == "R":
+                if _buffer[-1] == 'R':
                     break
         finally:
             termios.tcsetattr(_stdin, termios.TCSANOW, _tattr)
