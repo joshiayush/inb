d51b42a8df4054d5462e94b6874441df860ab332
Ayush Joshi <ayush854032@gmail.com>
2021-09-11 16:17:17 +0530

    feat(logger): create logger for command module
    create logger for command module
    log to the standard output stream file

--- inb/lib/command.py
+++ inb/lib/command.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-"""from __future__ imports must occur at the beginning of the file. DO NOT CHANGE!"""
+# from __future__ imports must occur at the beginning of the file. DO NOT CHANGE!
 from __future__ import annotations
 
 import logging
@@ -30,19 +30,14 @@ from typing import List
 
 from selenium.common.exceptions import TimeoutException
 
-from console.print import inbprint
-
-from errors import EmptyResponseException
-from errors import InternetNotConnectedException
-from errors import DomainNameSystemNotResolveException
-
 from lib import ping
+from errors import InternetNotConnectedException
 
 from linkedin import Driver
-from linkedin.linkedin import LinkedIn
-from linkedin.linkedinconnect import LinkedInConnect
-from linkedin.linkedinconnectviaid import LinkedInConnectViaId
-from linkedin.linkedinsearchconnect import LinkedInSearchConnect
+from linkedin.login import LinkedIn
+from linkedin.connect import LinkedInConnect
+from linkedin.connect import LinkedInConnectViaId
+from linkedin.connect import LinkedInSearchConnect
 
 from . import DRIVER_PATH
 from .commandhelper import CommandAide
@@ -52,16 +47,19 @@ class Command(CommandAide):
 
     def __init__(self: Command, namespace: argparse.Namespace) -> None:
         super().__init__(namespace=namespace)
+
         logging.basicConfig(
             format="%(levelname)s:%(message)s", level=logging.INFO)
-        logging.info("Checking network status")
+        self.logger = logging.getLogger("inb")
+        self.logger.setLevel(logging.DEBUG)
+
+        self.logger.info("Checking networ status")
         if not ping("linkedin.com"):
             raise InternetNotConnectedException("Weak network found")
-        logging.info("Strong network found")
+        self.logger.info("Internet is connected")
 
     def send(self: Command) -> None:
         chrome_driver_options: List[str] = []
-
         chrome_driver_options.append(Driver.INCOGNITO)
         chrome_driver_options.append(Driver.IGNORE_CERTIFICATE_ERRORS)
         if self.headless == True:
@@ -72,38 +70,30 @@ class Command(CommandAide):
                             driver_path=DRIVER_PATH,
                             opt_chromedriver_options=chrome_driver_options)
 
-        logging.info("Connecting")
+        self.logger.info("Connecting")
+        self.logger.info("Sending GET request to login page")
+        self.logger.info("Logging in LinkedIn account (user-id=%(user_id)s)" %
+                         {"user_id": self.email})
         try:
-            logging.info("Sending GET request to login page")
-            linkedin.get_login_page()
-            logging.info("Connected to login page")
-        except DomainNameSystemNotResolveException as error:
-            logging.critical(error)
+            linkedin.login()
+        except TimeoutException as error:
+            self.logger.critical(error)
             return
+        else:
+            self.logger.info("Successfully connected")
 
-        logging.info("Logging in LinkedIn account (user-id=%(user_id)s)" %
-                     {"user_id": self.email})
-        linkedin.login()
-        logging.info("Successfully connected")
-
-        logging.info("Instantiating connection object")
+        self.logger.info("Instantiating connection object")
         linkedin_connection = LinkedInConnect(driver=linkedin.driver,
                                               limit=self.limit)
-
+        self.logger.info("Sending GET request to mynetwork page")
         try:
-            logging.info("Sending GET request to mynetwork page")
-            linkedin_connection.get_my_network()
-            logging.info("Successfully connected to mynetwork page")
-        except EmptyResponseException as error:
-            logging.critical(error)
+            linkedin_connection.run()
+        except TimeoutException as error:
+            self.logger.critical(error)
             return
-
-        logging.info("Starting sending invitation")
-        linkedin_connection.run()
 
     def connect(self: Command) -> None:
         chrome_driver_options: List[str] = []
-
         chrome_driver_options.append(Driver.INCOGNITO)
         chrome_driver_options.append(Driver.IGNORE_CERTIFICATE_ERRORS)
         if self.headless == True:
@@ -113,39 +103,32 @@ class Command(CommandAide):
                             user_password=self.password,
                             driver_path=DRIVER_PATH,
                             opt_chromedriver_options=chrome_driver_options)
-        logging.info("Connecting")
+        self.logger.info("Connecting")
+        self.logger.info("Sending GET request to login page")
+        self.logger.info("Logging in LinkedIn account (user-id=%(user_id)s)" %
+                         {"user_id": self.email})
         try:
-            logging.info("Sending GET request to login page")
-            linkedin.get_login_page()
-            logging.info("Connected to login page")
-        except DomainNameSystemNotResolveException as error:
-            logging.critical(error)
+            linkedin.login()
+        except TimeoutException as error:
+            self.logger.critical(error)
             return
-        logging.info("Logging in LinkedIn account (user-id=%(user_id)s)" %
-                     {"user_id": self.email})
-        linkedin.login()
-        logging.info("Successfully connected")
+        else:
+            self.logger.info("Successfully connected")
 
-        logging.info("Instantiating connection object")
+        self.logger.info("Instantiating connection object")
         linkedin_connect_id = LinkedInConnectViaId(driver=linkedin.driver,
                                                    person_id=self.personid)
+        self.logger.info("Sending GET request to person's profile page")
         try:
-            logging.info("Sending GET request to person's profile page")
-            linkedin_connect_id.get_person_profile()
-            logging.info("Person's profile page received")
+            linkedin_connect_id.run()
         except TimeoutException as exc:
-            logging.critical(exc)
+            self.logger.critical(exc)
             return
-        logging.info("Starting sending invitation to person with profile id %(id)s" % {
-                     "id": self.profileid})
-        linkedin_connect_id.run()
 
     def search(self: Command) -> None:
-        chrome_driver_options: list = []
-
+        chrome_driver_options: List[str] = []
         chrome_driver_options.append(Driver.INCOGNITO)
         chrome_driver_options.append(Driver.IGNORE_CERTIFICATE_ERRORS)
-
         if self.headless == True:
             chrome_driver_options.append(Driver.HEADLESS)
 
@@ -154,20 +137,19 @@ class Command(CommandAide):
                             driver_path=DRIVER_PATH,
                             opt_chromedriver_options=chrome_driver_options)
 
-        inbprint("Connecting ...", color="cyan", blink=True, end="\r")
-
+        self.logger.info("Connecting")
+        self.logger.info("Sending GET request to login page")
+        self.logger.info("Logging in LinkedIn account (user-id=%(user_id)s)" %
+                         {"user_id": self.email})
         try:
-            linkedin.get_login_page()
-        except DomainNameSystemNotResolveException as exc:
-            inbprint(' '*80, end='\r')
-            inbprint(f"{exc}", color="red", bold=True)
+            linkedin.login()
+        except TimeoutException as error:
+            self.logger.critical(error)
             return
+        else:
+            self.logger.info("Successfully connected")
 
-        linkedin.login()
-
-        inbprint(' '*80, end='\r')
-        inbprint(f"Connected ✔", color="green", bold=True)
-
+        self.logger.info("Instantiating connection object")
         linkedin_search_connect = LinkedInSearchConnect(driver=linkedin.driver,
                                                         keyword=self.keyword,
                                                         location=self.location,
@@ -179,32 +161,17 @@ class Command(CommandAide):
                                                         current_company=self.current_company,
                                                         profile_language=self.profile_language,
                                                         limit=self.limit)
+        self.logger.info("Sending GET request to search results page")
         linkedin_search_connect.run()
 
     def show(self: Command) -> None:
-        """Show the table formed by the CommandHelper() class."""
-        pass
+        self.logger.info("facility not present")
 
     def config(self: Command) -> None:
-        """Throw the email and password in the database."""
-
-        """Ask user to enter its email and password."""
-        pass
+        self.logger.info("facility not present")
 
     def delete(self: Command) -> None:
-        """Delete the given value from the database."""
-
-        """Check what the user wants to delete."""
-
-        """Delete information from the database."""
-
-        """Dump database if user wants so."""
-        pass
+        self.logger.info("facility not present")
 
     def developer(self: Command) -> None:
-        """Show developer details."""
-
-        """Check what flag is passed and then throw the information"""
-
-        """Print every information if no flag is used."""
-        pass
+        self.logger.info("facility not present")
