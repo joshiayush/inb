c3334e99af11aeeff400b129cd983498caafaf84
Ayush Joshi <ayush854032@gmail.com>
2021-12-14 06:31:18 +0530

    Use `_GetGoogleChromeBinaryVersion()` function to return the version string independent of the platform
    Use `_GetGoogleChromeBinaryVersion()` function to return the version string independent
    of the platform to download `chromedriver` with supported version
    
    This feature enables compatibility with `windows`, `macOS` and `linux` with different
    `Google Chrome` versions
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- inb/linkedin/settings.py
+++ inb/linkedin/settings.py
@@ -20,8 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import re
 import os
+import sys
+import logging
 import pathlib
+import subprocess
 
 LOG_DIR_PATH = pathlib.Path(__file__).resolve().parent.parent.parent / 'logs'
 
@@ -34,6 +38,82 @@ if not os.path.exists(LOG_DIR_PATH):
 LOG_FORMAT_STR = '%(asctime)s:%(name)s:%(levelname)s:%(filename)s:%(funcName)s:%(message)s'  # pylint: disable=line-too-long
 
 INB_VERSION = '1.0.0'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+file_handler = logging.FileHandler(LOG_DIR_PATH / __name__, mode='w')
+file_handler.setFormatter(logging.Formatter(LOG_FORMAT_STR))
+
+stream_handler = logging.StreamHandler(sys.stderr)
+stream_handler.setFormatter(logging.Formatter(LOG_FORMAT_STR))
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+
+def _GetGoogleChromeBinaryVersion() -> str:
+  version_regex = r'[0-9]{2}.[0-9]{1}.[0-9]{4}.[0-9]{2}'
+  if sys.platform == 'linux':
+    chrome_binaries = ['google-chrome', 'google-chrome-stable']
+    chrome_binary_path = []
+    for binary in chrome_binaries:
+      try:
+        chrome_binary_path.append(
+            subprocess.check_output(['whereis', '-b',
+                                     binary]).decode('utf-8')[len(binary) +
+                                                              1::].strip())
+      except subprocess.CalledProcessError as exc:
+        logger.error(('CalledProcessError: Exit code %d.'
+                      '\n%s.'), exc.returncode, exc.output)
+        continue
+    for i in range(len(chrome_binary_path)):
+      if chrome_binary_path[i] == '':
+        chrome_binary_path = chrome_binary_path[0:i:] + chrome_binary_path[i +
+                                                                           1::]
+    for path in chrome_binary_path:
+      try:
+        version = subprocess.check_output([path, '--version']).decode('utf-8')
+      except subprocess.CalledProcessError:
+        logger.error('Could not find Chrome binary in path %s.', path)
+      else:
+        version = re.search(version_regex, version)
+        return version.group(0)
+    raise FileNotFoundError(('Could not find Chrome binary in following paths,'
+                             f'\n{chrome_binary_path}'))
+  elif sys.platform == 'darwin':
+    chrome_binary_path = (
+        r'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+    for path in chrome_binary_path:
+      try:
+        version = subprocess.check_output([path, '--version']).decode('utf-8')
+      except subprocess.CalledProcessError:
+        logger.error('Could not find Chrome binary in path %s.', path)
+      else:
+        version = re.search(version_regex, version)
+        return version.group(0)
+    raise FileNotFoundError(('Could not find Chrome binary in following paths,'
+                             f'\n{chrome_binary_path}'))
+  elif sys.platform in ('win32', 'cygwin'):
+    chrome_binary_path = (
+        r'%ProgramFiles%\Google\Chrome\Application\chrome.exe',
+        r'%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe',
+        r'%LocalAppData%\Google\Chrome\Application\chrome.exe',
+        r'C:\Users\USER\AppData\Local\Google\Chrome\Application\chome.exe')
+    for path in chrome_binary_path:
+      try:
+        version = subprocess.check_output([
+            'wmic', 'datafile', 'where', f'name={path}', 'get', 'Version',
+            '/value'
+        ]).decode('utf-8')
+      except subprocess.CalledProcessError:
+        logger.error('Could not find Chrome binary in path %s.', path)
+        continue
+      else:
+        version = re.search(version_regex, version)
+        return version.group(0)
+    raise FileNotFoundError((f'Could not find Chrome binary in following paths,'
+                             f'\n{chrome_binary_path}'))
 
 
 def _ChromeDriverAbsPath() -> str:
