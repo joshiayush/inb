c83afbbf143c1c2578174f63c0b74e7016d57a63
Ayush Joshi <ayush854032@gmail.com>
2021-12-21 17:53:27 +0530

    feat(invitation): add extra information in `send` and `search` command's invitaiton status
    use template  `SEND_INVITATION_STATUS_TEMPL` to generate invitation status for `send`
    command by filling the template with the actual information scraped from the page
    
    use template `SEARCH_INVITATION_STATUS_TEMPL` to generate invitation status for `search`
    command by filling the template with the actual information scraped from the page
    
    fixes https://github.com/joshiayush/inb/issues/33 https://github.com/joshiayush/inb/issues/34
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- inb/linkedin/invitation/status.py
+++ inb/linkedin/invitation/status.py
@@ -1,92 +1,133 @@
-# MIT License
+# Copyright 2021, joshiayus Inc.
+# All rights reserved.
 #
-# Copyright (c) 2019 Creative Commons
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of joshiayus Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# from __future__ imports must occur at the beginning of the file. DO NOT CHANGE!
 from __future__ import annotations
 
 import time
 
-from typing import (
-  Any,
-  Dict,
-)
+SUCCESS_RATE = 0
+FAILURE_RATE = 0
 
-from linkedin.invitation import (
-  SUCCESS_RATE,
-  FAILURE_RATE,
-  SENT_STATUS_SYMBOL,
-  FAILED_STATUS_SYMBOL,
-  UNDEFINED_STATUS_SYMBOL,
-)
+SENT_STATUS_SYMBOL = '✔'
+FAILED_STATUS_SYMBOL = '✘'
+
+SEND_INVITATION_STATUS_TEMPL = """  {{status}}  {{name}}
+  {{occupation}}
+  {{mutual_connections}}
+  ID: {{profileid}}
+  URL: {{profileurl}}
+  Success:  {{success}}  Failure: {{failure}}  Elapsed time: {{elapsed_time}}
+"""
+
+SEARCH_INVITATION_STATUS_TEMPL = """  {{status}}  {{name}}  • {{degree}}
+  {{occupation}}
+  {{location}}
+  {{summary}}
+  {{mutual_connections}}
+  ID: {{profileid}}
+  URL: {{profileurl}}
+  Success:  {{success}}  Failure: {{failure}}  Elapsed time: {{elapsed_time}}
+"""
 
 
 class Invitation(object):
-  STATUS_NO: int = 0
-  SLEEP_TIME_AFTER_LOGGING: int | float = 0.18
+  _SLEEP_TIME_AFTER_LOGGING = 0.18
 
-  def __init__(self: Invitation, **kwargs: Dict[Any, Any]) -> None:
-    self.set_invitation_fields(**kwargs)
-
-  def set_invitation_fields(self: Invitation,
-                            name: str = '',
-                            occupation: str = '',
-                            status: str = '',
-                            elapsed_time: int | float = 0) -> None:
+  def set_invitation_fields(self, name: str, occupation: str, location: str,
+                            summary: str, mutual_connections: str,
+                            profileid: str, profileurl: str, status: str,
+                            elapsed_time: int) -> None:
     self._name = name
-    if len(occupation) >= 50:
-      self._occupation = occupation[0:50] + ' ' + '.' * 3
-    else:
-      self._occupation = occupation
+    self._occupation = occupation
+    self._location = location
+    self._summary = summary
+    self._mutual_connections = mutual_connections
+    self._profileid = profileid
+    self._profileurl = profileurl
+
     self._success_rate = 0
     self._failure_rate = 0
-    if status == "sent":
+
+    if status == 'sent':
       self._status = SENT_STATUS_SYMBOL
       global SUCCESS_RATE
       SUCCESS_RATE += 1
       self._success_rate = SUCCESS_RATE
-    elif status == "failed":
+    elif status == 'failed':
       self._status = FAILED_STATUS_SYMBOL
       global FAILURE_RATE
       FAILURE_RATE += 1
       self._failure_rate = FAILURE_RATE
-    else:
-      self._status = UNDEFINED_STATUS_SYMBOL
+
     try:
-      self._elapsed_time = str(elapsed_time)[0:5] + "s"
+      self._elapsed_time = str(elapsed_time)[0:5] + 's'
     except IndexError:
       self._elapsed_time = elapsed_time
 
-  def status(self: Invitation, come_back_by: int = 8) -> None:
-    if Invitation.STATUS_NO > 0:
-      print("\033[F" * come_back_by)
-    else:
-      Invitation.STATUS_NO += 1
-    print()
-    print(
-        f"""{self._status} {self._name}\n\n"""
-        f"""{self._occupation}\n\n"""
-        f"""Success: {self._success_rate} Failed: {self._failure_rate} Elapsed: {self._elapsed_time}""")
-    print()
-    time.sleep(self.SLEEP_TIME_AFTER_LOGGING)
+  def _fill_send_message_template(self) -> str:
+    message = SEND_INVITATION_STATUS_TEMPL
+    message = message.replace('{{status}}', self._status)
+    message = message.replace('{{name}}', self._name)
+    message = message.replace('{{occupation}}', self._occupation)
+    message = message.replace('{{mutual_connections}}',
+                              self._mutual_connections)
+    message = message.replace('{{profileid}}', self._profileid)
+    message = message.replace('{{profileurl}}', self._profileurl)
+    message = message.replace('{{success}}', str(self._success_rate))
+    message = message.replace('{{failure}}', str(self._failure_rate))
+    message = message.replace('{{elapsed_time}}', str(self._elapsed_time))
+    return message
 
-  def __del__(self: Invitation) -> None:
-    Invitation.STATUS_NO = 0
+  def _fill_search_message_template(self) -> str:
+    message = SEARCH_INVITATION_STATUS_TEMPL
+    message = message.replace('{{status}}', self._status)
+    message = message.replace('{{name}}', self._name)
+    message = message.replace('{{occupation}}', self._occupation)
+    message = message.replace('{{location}}', self._location)
+    message = message.replace('{{summary}}', self._summary)
+    message = message.replace('{{mutual_connections}}',
+                              self._mutual_connections)
+    message = message.replace('{{profileid}}', self._profileid)
+    message = message.replace('{{profileurl}}', self._profileurl)
+    message = message.replace('{{success}}', str(self._success_rate))
+    message = message.replace('{{failure}}', str(self._failure_rate))
+    message = message.replace('{{elapsed_time}}', str(self._elapsed_time))
+    return message
+
+  def status(self) -> None:
+    if self._location is None and self._summary is None:
+      message = self._fill_send_message_template()
+    else:
+      message = self._fill_search_message_template()
+
+    print()
+    print(message)
+    print()
+
+    time.sleep(self._SLEEP_TIME_AFTER_LOGGING)
