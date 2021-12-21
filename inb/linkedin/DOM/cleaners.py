0362d27ae96abd8ee213e46ef59dcdc821570ba2
Ayush Joshi <ayush854032@gmail.com>
2021-12-21 17:59:23 +0530

    feat(DOM): add logger to `Cleaner` API and add function `load_page()` to `JS` API
    consider logging traceback in function `clear_message_overlay()` in case there's an
    exception also consider separating the element's selectors in their own type
    
    consider using a single function to load a dynamically-loading-page and use global
    variables `_GET_PAGE_Y_OFFSET_SCRIPT` and `_SCROLL_TO_BOTTOM_SCRIPT` to obtain the page
    y offset and to scroll the page to the bottom respectively
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- inb/linkedin/DOM/cleaners.py
+++ inb/linkedin/DOM/cleaners.py
@@ -1,184 +1,89 @@
-"""
-This module provides APIs to clean elements from document by setting their `display`
-property to `none` while sending invitation and re-setting their `display` property to
-normal before deleting the current instance.
+# Copyright 2021, joshiayus Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of joshiayus Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-  :author: Ayush Joshi, ayush854032@gmail.com
-  :copyright: Copyright (c) 2019 Creative Commons.
-  :license: MIT License, see license for details.
-"""
-
-# MIT License
-#
-# Copyright (c) 2019 Creative Commons
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-# from __future__ imports must occur at the beginning of the file. DO NOT CHANGE!
 from __future__ import annotations
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import sys
+import logging
+import traceback
 
+from linkedin import (driver, settings)
+
+from selenium.common import exceptions
+from selenium.webdriver.common import by
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from urllib3.exceptions import MaxRetryError
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.CRITICAL)
 
-from selenium.common.exceptions import (
-  TimeoutException,
-  NoSuchElementException,
-)
+file_handler = logging.FileHandler(settings.LOG_DIR_PATH / __name__, mode='w')
+file_handler.setFormatter(logging.Formatter(settings.LOG_FORMAT_STR))
 
-FUNC_GET_ELEMENT_BY_XPATH = """
+if settings.LOGGING_TO_STREAM_ENABLED:
+  stream_handler = logging.StreamHandler(sys.stderr)
+  stream_handler.setFormatter(logging.Formatter(settings.LOG_FORMAT_STR))
+  logger.addHandler(stream_handler)
+
+logger.addHandler(file_handler)
+
+_FUNC_GET_ELEMENT_BY_XPATH_STR = """
 function getElementByXpath(path) {
-  return document.evaluate(
-    path, 
-    document, 
-    null, 
-    XPathResult.FIRST_ORDERED_NODE_TYPE, 
-    null
-  ).singleNodeValue;
+  return document.evaluate(path, document, null, 
+    XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 """
 
 
-class Cleaner(object):
-  WAIT = 60
-  MSG_OVERLAY_XPATH = "//*[@id='msg-overlay']"
+class _CleanerElementsSelectors:
 
-  def __init__(self: Cleaner, driver: webdriver.Chrome) -> None:
-    """Constructor method to initialize a Cleaner object.
+  @staticmethod
+  def get_message_overlay_css_selector() -> str:
+    return "div[class^='msg-overlay-list-bubble']"
 
-    Args:
-      self: (Cleaner) Self.
-      driver: (webdriver.Chrome) Chromedriver.
+  @staticmethod
+  def get_message_overlay_xpath() -> str:
+    return "//*[@id='msg-overlay']"
 
-    Raises:
-      Exception: If 'driver' not given.
 
-    Example:
-    >>> from DOM import Cleaner
-    >>> from selenium import webdriver
-    >>>
-    >>> chrome = webdriver.Chrome('your/chromedriver/path')
-    >>> cleaner = Cleaner(chrome)
+class Cleaner:
 
-    Variable `elements_marked_none` holds the elements the current instance
-    marked as `display: none;`; this way before the instance dies we re-sets
-    the 'display' property of those elements so if in case any process requires
-    those elements in future it will not throw any kind of `NoSuchElementException`
-    exception.
-
-    Prototype:
-    >>> [
-    ...  {
-    ...    element: webdriver.Chrome,
-    ...    selector: str,
-    ...    method: 'css|xpath|...',
-    ...    original_display_value: str
-    ...  }
-    ... ]
-    """
-    if not driver:
-      raise Exception('webdriver.Chrome instance is not given!')
-    self._driver = driver
-    self.elements_marked_none = []
-
-  def clear_message_overlay(self: Cleaner, wait: int = None) -> None:
-    """Method `clear_message_overlay()` clears the message overlay that overlaps the
-    page. This is important as while sending invitation `selenium` will throw `exception`
-    incase any element is overlapping the `invite` buttons.
-
-    Args:
-      self: (Cleaner) Self.
-      wait: (int) Timeout.
-
-    Example:
-    >>> from DOM import Cleaner
-    >>> from selenium import webdriver
-    >>>
-    >>> chrome = webdriver.Chrome('your/chromedriver/path')
-    >>> cleaner = Cleaner(chrome)
-    >>> cleaner.clear_message_overlay() # this will clear the message overlay lying of the screen
-    """
-    if not wait:
-      wait = Cleaner.WAIT
-
+  @staticmethod
+  def clear_message_overlay(wait: int = 60) -> None:
     try:
-      element = WebDriverWait(
-          self._driver, wait).until(
+      WebDriverWait(driver.GetGlobalChromeDriverInstance(), wait).until(
           EC.presence_of_element_located(
-            (By.CSS_SELECTOR,
-             "div[class^='msg-overlay-list-bubble']")
-          )
-      )
-      selector = Cleaner.MSG_OVERLAY_XPATH
-      method = 'xpath'
-      original_display_value = element.value_of_css_property(
-          'display')
-      self._driver.execute_script(f"""
-        {FUNC_GET_ELEMENT_BY_XPATH}
-        getElementByXpath(`{Cleaner.MSG_OVERLAY_XPATH}`).style = "display: none;";
+              (by.By.CSS_SELECTOR,
+               _CleanerElementsSelectors.get_message_overlay_css_selector())))
+      driver.GetGlobalChromeDriverInstance().execute_script(f"""
+        {_FUNC_GET_ELEMENT_BY_XPATH_STR}
+        getElementByXpath(`{_CleanerElementsSelectors.get_message_overlay_xpath()}`).style = "display: none;";
       """)
-    except (NoSuchElementException, TimeoutException):
-      return
-
-    self.elements_marked_none.append(
-      {'element': element, 'selector': selector, 'method': method,
-       'original_display_value': original_display_value})
-
-  def __del__(self: Cleaner) -> None:
-    """Destructor re-sets elements original state before deleting current instance.
-
-    This is neccessary as if in case any other process wants to access the element
-    that we removed earlier, selenium will throw `NoSuchElementException` as it won't
-    be able to locate that element as we've already removed it from the document.
-
-    This destructor method takes care of the above issue from happening by re-setting
-    every elements' state back to normal.
-
-    Args:
-      self: (Cleaner) Self.
-
-    Example:
-    >>> from DOM import Cleaner
-    >>> from selenium import webdriver
-    >>>
-    >>> chrome = webdriver.Chrome('your/chromedriver/path')
-    >>> cleaner = Cleaner(chrome)
-    >>>
-    >>> # this will clear the message overlay lying of the screen by setting display to none
-    >>> cleaner.clear_message_overlay() 
-    >>> # this will set the message overlay item to its original state
-    >>> del cleaner
-    """
-    try:
-      for element in self.elements_marked_none:
-        if element['method'] == 'xpath':
-          self._driver.execute_script(f"""
-            {FUNC_GET_ELEMENT_BY_XPATH}
-            getElementByXpath(`{element['selector']}`).style = "display: {element['original_display_value']};";
-          """)
-        elif element['method'] == 'css':
-          self._driver.execute_script(f"""
-            document.querySelector(`{element['selector']}`).style = "display: {element['original_display_value']}"
-            """)
-    except MaxRetryError:
-      pass
+    except (exceptions.NoSuchElementException, exceptions.TimeoutException):
+      logger.critical(traceback.format_exc())
--- inb/linkedin/DOM/javascript.py
+++ inb/linkedin/DOM/javascript.py
@@ -1,100 +1,62 @@
-"""
-This module provides APIs to work with document's page y offsets.
+# Copyright 2021, joshiayus Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of joshiayus Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-  :author: Ayush Joshi, ayush854032@gmail.com
-  :copyright: Copyright (c) 2019 Creative Commons.
-  :license: MIT License, see license for details.
-"""
-
-# MIT License
-#
-# Copyright (c) 2019 Creative Commons
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-# from __future__ imports must occur at the beginning of the file. DO NOT CHANGE!
 from __future__ import annotations
 
-from selenium import webdriver
+import time
+
+from linkedin import driver
+
+_GET_PAGE_Y_OFFSET_SCRIPT = (
+    'return (window.pageYOffset !== undefined)'
+    '  ? window.pageYOffset'
+    '  : (document.documentElement || document.body.parentNode || document.body);'  # pylint: disable=line-too-long
+)
+
+_SCROLL_TO_BOTTOM_SCRIPT = (
+    'var scrollingElement = (document.scrollingElement || document.body);'
+    'scrollingElement.scrollTop = scrollingElement.scrollHeight;')
 
 
 class JS:
-  def __init__(self: JS, driver: webdriver.Chrome) -> None:
-    """Constructor method to initialize a Cleaner object.
+  """Utility JavaScript scripts to use at runtime."""
 
-    Args:
-      self: (JS) Self.
-      driver: (webdriver) Chromedriver
-
-    Raises:
-      Exception: If driver is not given.
-
-    Example:
-    >>> from DOM import JS
-    >>> from selenium import webdriver
-    >>>
-    >>> js = JS(webdriver.Chrome())
-    >>> page_offset = js.get_page_y_offset()
-    """
-    if not driver:
-      raise Exception('webdriver.Chrome instance is not given!')
-    self._driver = driver
-
-  def get_page_y_offset(self: JS) -> int:
-    """Method `get_page_y_offset()` returns the `window.pageYOffset` of the
-    webpage, we need that so we can keep on scrolling untill the page offset
-    becomes constant.
-
-    Args:
-      self: (object) Self.
-
-    Returns:
-      window.pageYOffset
-
-    Example:
-    >>> from DOM import JS
-    >>> from selenium import webdriver
-    >>>
-    >>> js = JS(webdriver.Chrome())
-    >>> page_offset = js.get_page_y_offset()
-    """
-    return self._driver.execute_script((
-      "return (window.pageYOffset !== undefined)"
-      "  ? window.pageYOffset"
-      "  : (document.documentElement || document.body.parentNode || document.body);"
-    ))
-
-  def scroll_bottom(self: JS) -> None:
-    """Method `scroll_bottom()` scrolls the web page to the very bottom of
-    it using the `document.scrollingElement.scrollTop` property.
-
-    Args:
-      self: (JS) Self.
-
-    Example:
-    >>> from DOM import JS
-    >>> from selenium import webdriver
-    >>>
-    >>> js = JS(webdriver.Chrome())
-    >>> js.scroll_bottom()
-    """
-    self._driver.execute_script((
-      "var scrollingElement = (document.scrollingElement || document.body);"
-      "scrollingElement.scrollTop = scrollingElement.scrollHeight;"
-    ))
+  @staticmethod
+  def load_page() -> None:
+    """Loads the page the bot is currently at by scrolling it down."""
+    old_page_offset = driver.GetGlobalChromeDriverInstance().execute_script(
+        _GET_PAGE_Y_OFFSET_SCRIPT)
+    new_page_offset = driver.GetGlobalChromeDriverInstance().execute_script(
+        _GET_PAGE_Y_OFFSET_SCRIPT)
+    while old_page_offset == new_page_offset:
+      driver.GetGlobalChromeDriverInstance().execute_script(
+          _SCROLL_TO_BOTTOM_SCRIPT)
+      time.sleep(1)
+      new_page_offset = driver.GetGlobalChromeDriverInstance().execute_script(
+          _GET_PAGE_Y_OFFSET_SCRIPT)
