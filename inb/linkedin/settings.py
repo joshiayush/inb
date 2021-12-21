8ed370a7114cd3571781a59e111b5f86d5489923
Ayush Joshi <ayush854032@gmail.com>
2021-12-21 17:46:07 +0530

    Remove `debug` setting and add path to `LinkedIn` pages
    We are not going to produce debugging information from our program we can just simply
    stream the logging information to the console using the `TurnOnLoggingToStream()`
    function
    
    Also, add urls to `LinkedIn` pages
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- inb/linkedin/settings.py
+++ inb/linkedin/settings.py
@@ -1,24 +1,31 @@
-# MIT License
+# Copyright 2021, joshiayus Inc.
+# All rights reserved.
 #
-# Copyright (c) 2019 Creative Commons
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of joshiayus Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import re
 import os
@@ -28,14 +35,6 @@ import pathlib
 import subprocess
 
 LOG_DIR_PATH = pathlib.Path(__file__).resolve().parent.parent.parent / 'logs'
-
-# Variable's value decides whether debugging is allowed in the entire project.
-#
-# Note: You must not update the value of this variable directly, you must call
-# the `TurnOnLoggingLevelDebug()` function to update its value otherwise you may
-# update the value of this variable but this particular module will not have any
-# effect of that change.
-LOGGING_LEVEL_DEBUG_ENABLED = False
 
 # Variable's value decides whether logging to stream is allowed in the entire
 # project.
@@ -72,12 +71,6 @@ def TurnOnLoggingToStream() -> None:
   logger.addHandler(stream_handler)
 
 
-def TurnOnLoggingLevelDebug() -> None:
-  global LOGGING_LEVEL_DEBUG_ENABLED
-  LOGGING_LEVEL_DEBUG_ENABLED = True
-  logger.setLevel(logging.DEBUG)
-
-
 _CHROME_BINARY_NOT_FOUND_MSG = 'Google Chrome binary is not present in path %s.'
 _CHROME_BINARIES_NOT_FOUND_MSG = (
     'Google Chrome binary is not present in the following paths\n'
@@ -108,6 +101,7 @@ def _GetGoogleChromeBinaryVersion() -> str:
         version = subprocess.check_output([path, '--version']).decode('utf-8')
       except subprocess.CalledProcessError:
         logger.error(_CHROME_BINARY_NOT_FOUND_MSG, path)
+        continue
       else:
         version = re.search(version_regex, version)
         return version.group(0)
@@ -121,6 +115,7 @@ def _GetGoogleChromeBinaryVersion() -> str:
         version = subprocess.check_output([path, '--version']).decode('utf-8')
       except subprocess.CalledProcessError:
         logger.error(_CHROME_BINARY_NOT_FOUND_MSG, path)
+        continue
       else:
         version = re.search(version_regex, version)
         return version.group(0)
@@ -148,10 +143,19 @@ def _GetGoogleChromeBinaryVersion() -> str:
                             (', '.join(chrome_binary_path)))
 
 
-def _ChromeDriverAbsPath() -> str:
+def ChromeDriverAbsPath() -> str:
   dir_path = os.path.dirname(os.path.abspath(__file__))
   last_inb_indx = dir_path.rfind('inb')
   return os.path.join(dir_path[:last_inb_indx], 'driver/chromedriver')
 
 
-CHROME_DRIVER_ABS_PATH = _ChromeDriverAbsPath()
+def GetLinkedInUrl() -> str:
+  return 'https://www.linkedin.com'
+
+
+def GetLinkedInLoginPageUrl() -> str:
+  return GetLinkedInUrl() + '/login/'
+
+
+def GetLinkedInMyNetworkPageUrl() -> str:
+  return GetLinkedInUrl() + '/mynetwork/'
