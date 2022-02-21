65766c9a37a1cb8fedeeca932976402b82a79a1b
Ayush Joshi <ayush854032@gmail.com>
2022-02-21 11:07:51 +0530

    chore: remove `setup.py` until `linkedin` is shipped as a package
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name="inb",
-      version="1.51.35",
-      description="inb is a command line tool to automate LinkedIn. Achieve 500+ connections very easily you just need to try it.",
-      url="https://github.com/JoshiAyush/inb",
-      license="MIT",
-      author="Ayush Joshi",
-      author_email="ayush854032@gmail.com")
