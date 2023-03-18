c4be2e5a4ca68a7e75dcc483e3277a5080ab964b
joshiayush <ayush854032@gmail.com>
2023-03-18 21:32:20 +0530

    Serve `inb` under `Apache` license 2.0
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- inb/tests/test_cookierepo.py
+++ inb/tests/test_cookierepo.py
@@ -1,33 +1,18 @@
-# pylint: disable=missing-module-docstring
+# pylint: disable=missing-module-docstring, protected-access
 
-# Copyright 2021, joshiayus Inc.
-# All rights reserved.
+# Copyright 2023 The inb Authors. All Rights Reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of joshiayus Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import time
@@ -62,10 +47,10 @@ class TestCookieRepository:
   def test__get_cookies_jar_file_path(self):
     expected_path = os.path.join(self.tmp_dir, self.username)
     assert os.fspath(
-        self.cookie_repo._get_cookies_jar_file_path()) == expected_path  # pylint: disable=protected-access
+        self.cookie_repo._get_cookies_jar_file_path()) == expected_path
 
   def test_save(self):
-    cookie_jar_file_path = self.cookie_repo._get_cookies_jar_file_path()  # pylint: disable=protected-access
+    cookie_jar_file_path = self.cookie_repo._get_cookies_jar_file_path()
     assert not os.path.exists(cookie_jar_file_path)
     self.cookie_repo.save()
     assert os.path.exists(cookie_jar_file_path)
@@ -75,7 +60,7 @@ class TestCookieRepository:
     assert loaded_cookie_jar == self.cookie_jar
 
   def test_get_cookies(self):
-    cookie_jar_file_path = self.cookie_repo._get_cookies_jar_file_path()  # pylint: disable=protected-access
+    cookie_jar_file_path = self.cookie_repo._get_cookies_jar_file_path()
     with open(cookie_jar_file_path, 'wb') as jar_file:
       pickle.dump(self.cookie_jar, jar_file)
 
--- inb/tests/test_invitation_status.py
+++ inb/tests/test_invitation_status.py
@@ -1,33 +1,18 @@
-# pylint: disable=missing-module-docstring
+# pylint: disable=missing-module-docstring, redefined-outer-name, protected-access
 
-# Copyright 2021, joshiayus Inc.
-# All rights reserved.
+# Copyright 2023 The inb Authors. All Rights Reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of joshiayus Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import sys
 import pytest
@@ -51,7 +36,7 @@ def invitation():
   return status.Invitation()
 
 
-def test_person_properties(person):  # pylint: disable=redefined-outer-name
+def test_person_properties(person):
   assert person.name == 'John Smith'
   assert person.occupation == 'Software Engineer'
   assert person.location == 'San Francisco, CA'
@@ -59,7 +44,7 @@ def test_person_properties(person):  # pylint: disable=redefined-outer-name
   assert person.profileurl == 'https://www.linkedin.com/in/john-smith'
 
 
-def test_invitation_set_invitation_fields_success(invitation):  # pylint: disable=redefined-outer-name
+def test_invitation_set_invitation_fields_success(invitation):
   invitation.set_invitation_fields(
       name='John Smith',
       occupation='Software Engineer',
@@ -69,18 +54,18 @@ def test_invitation_set_invitation_fields_success(invitation):  # pylint: disabl
       status='sent',
       elapsed_time=10.0)
 
-  assert invitation._name == 'John Smith'  # pylint: disable=protected-access
-  assert invitation._occupation == 'Software Engineer'  # pylint: disable=protected-access
-  assert invitation._location == 'San Francisco, CA'  # pylint: disable=protected-access
-  assert invitation._profileid == 'john-smith'  # pylint: disable=protected-access
-  assert invitation._profileurl == 'https://www.linkedin.com/in/john-smith'  # pylint: disable=protected-access
-  assert invitation._success_rate == 1  # pylint: disable=protected-access
-  assert invitation._failure_rate == 0  # pylint: disable=protected-access
-  assert invitation._status == '✔'  # pylint: disable=protected-access
-  assert invitation._elapsed_time == '10.0s'  # pylint: disable=protected-access
+  assert invitation._name == 'John Smith'
+  assert invitation._occupation == 'Software Engineer'
+  assert invitation._location == 'San Francisco, CA'
+  assert invitation._profileid == 'john-smith'
+  assert invitation._profileurl == 'https://www.linkedin.com/in/john-smith'
+  assert invitation._success_rate == 1
+  assert invitation._failure_rate == 0
+  assert invitation._status == '✔'
+  assert invitation._elapsed_time == '10.0s'
 
 
-def test_invitation_set_invitation_fields_failure(invitation):  # pylint: disable=redefined-outer-name
+def test_invitation_set_invitation_fields_failure(invitation):
   invitation.set_invitation_fields(
       name='John Smith',
       occupation='Software Engineer',
@@ -90,20 +75,20 @@ def test_invitation_set_invitation_fields_failure(invitation):  # pylint: disabl
       status='failed',
       elapsed_time=10.0)
 
-  assert invitation._name == 'John Smith'  # pylint: disable=protected-access
-  assert invitation._occupation == 'Software Engineer'  # pylint: disable=protected-access
-  assert invitation._location == 'San Francisco, CA'  # pylint: disable=protected-access
-  assert invitation._profileid == 'john-smith'  # pylint: disable=protected-access
-  assert invitation._profileurl == 'https://www.linkedin.com/in/john-smith'  # pylint: disable=protected-access
-  assert invitation._success_rate == 0  # pylint: disable=protected-access
-  assert invitation._failure_rate == 1  # pylint: disable=protected-access
-  assert invitation._status == '✘'  # pylint: disable=protected-access
-  assert invitation._elapsed_time == '10.0s'  # pylint: disable=protected-access
+  assert invitation._name == 'John Smith'
+  assert invitation._occupation == 'Software Engineer'
+  assert invitation._location == 'San Francisco, CA'
+  assert invitation._profileid == 'john-smith'
+  assert invitation._profileurl == 'https://www.linkedin.com/in/john-smith'
+  assert invitation._success_rate == 0
+  assert invitation._failure_rate == 1
+  assert invitation._status == '✘'
+  assert invitation._elapsed_time == '10.0s'
 
 
-def test_invitation_send_status_to_console(invitation):  # pylint: disable=redefined-outer-name
-  status._SUCCESS_RATE = 0  # pylint: disable=protected-access
-  status._FAILURE_RATE = 0  # pylint: disable=protected-access
+def test_invitation_send_status_to_console(invitation):
+  status._SUCCESS_RATE = 0
+  status._FAILURE_RATE = 0
 
   invitation.set_invitation_fields(
       name='John Smith',
@@ -120,7 +105,7 @@ def test_invitation_send_status_to_console(invitation):  # pylint: disable=redef
                      '  Success: 1  Failure: 0  Elapsed time: 10.0s\n')
 
   with mock.patch('click.echo') as mk_click_echo:
-    invitation._send_status_to_console()  # pylint: disable=protected-access
+    invitation._send_status_to_console()
     mk_click_echo.assert_has_calls([
         mock.call('', sys.stdout, True, True),
         mock.call(expected_output, sys.stdout, True, True),
@@ -136,12 +121,12 @@ def mock_sleep():
 
 
 def test_invitation_display_invitation_status_on_console(
-    invitation, mock_sleep):  # pylint: disable=redefined-outer-name
+    invitation, mock_sleep):
   # Test invitation sent
   with mock.patch('click.echo') as mk_echo:
-    status._SUCCESS_RATE = 0  # pylint: disable=protected-access
-    status._FAILURE_RATE = 0  # pylint: disable=protected-access
-    invitation.set_invitation_fields(  # pylint: disable=redefined-outer-name
+    status._SUCCESS_RATE = 0
+    status._FAILURE_RATE = 0
+    invitation.set_invitation_fields(
         name='John Smith',
         occupation='Software Engineer',
         location='San Francisco',
@@ -149,7 +134,7 @@ def test_invitation_display_invitation_status_on_console(
         profileurl='https://linkedin.com/john-smith',
         status='sent',
         elapsed_time=10.0)
-    invitation._send_status_to_console()  # pylint: disable=protected-access
+    invitation._send_status_to_console()
     expected_output = ('  ✔  John Smith\n'
                        '  Software Engineer\n'
                        '  San Francisco\n'
@@ -159,13 +144,13 @@ def test_invitation_display_invitation_status_on_console(
         mock.call(expected_output, sys.stdout, True, True),
         mock.call('', sys.stdout, True, True)
     ])
-    mock_sleep.assert_called_with(status.Invitation._SLEEP_TIME_AFTER_LOGGING)  # pylint: disable=protected-access
+    mock_sleep.assert_called_with(status.Invitation._SLEEP_TIME_AFTER_LOGGING)
 
   # Test invitation failed
   with mock.patch('click.echo') as mk_echo:
-    status._SUCCESS_RATE = 0  # pylint: disable=protected-access
-    status._FAILURE_RATE = 0  # pylint: disable=protected-access
-    invitation.set_invitation_fields(  # pylint: disable=redefined-outer-name
+    status._SUCCESS_RATE = 0
+    status._FAILURE_RATE = 0
+    invitation.set_invitation_fields(
         name='John Smith',
         occupation='Software Engineer',
         location='San Francisco',
@@ -173,7 +158,7 @@ def test_invitation_display_invitation_status_on_console(
         profileurl='https://linkedin.com/john-smith',
         status='failed',
         elapsed_time=10.0)
-    invitation._send_status_to_console()  # pylint: disable=protected-access
+    invitation._send_status_to_console()
     expected_output = ('  ✘  John Smith\n'
                        '  Software Engineer\n'
                        '  San Francisco\n'
@@ -183,4 +168,4 @@ def test_invitation_display_invitation_status_on_console(
         mock.call(expected_output, sys.stdout, True, True),
         mock.call('', sys.stdout, True, True)
     ])
-    mock_sleep.assert_called_with(status.Invitation._SLEEP_TIME_AFTER_LOGGING)  # pylint: disable=protected-access
+    mock_sleep.assert_called_with(status.Invitation._SLEEP_TIME_AFTER_LOGGING)
